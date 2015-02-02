919db9f8cf5108f049bd20192aeac1c7ee2b2afc
GreenAddress <github@greenaddress.it>
2015-02-02 16:01:43 +0100

    Quicker QR encoding, updated app.iml

--- app/src/main/java/com/greenaddress/greenbits/QrBitmap.java
+++ app/src/main/java/com/greenaddress/greenbits/QrBitmap.java
@@ -2,11 +2,16 @@ package com.greenaddress.greenbits;
 
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.util.Log;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.google.zxing.qrcode.encoder.ByteMatrix;
+import com.google.zxing.qrcode.encoder.Encoder;
+import com.google.zxing.qrcode.encoder.QRCode;
 
 import java.util.concurrent.Callable;
 
@@ -21,21 +26,23 @@ public class QrBitmap implements Callable<QrBitmap> {
         this.background_color = background_color;
     }
 
-    private static Bitmap toBitmap(final BitMatrix matrix, final int qrcode_color, final int background_color) {
-        final int height = matrix.getHeight();
-        final int width = matrix.getWidth();
+    private static Bitmap toBitmap(final QRCode code, final int qrcode_color, final int background_color) {
+        final ByteMatrix matrix = code.getMatrix();
+        final int SCALE = 4;
+        final int height = matrix.getHeight() * SCALE;
+        final int width = matrix.getWidth() * SCALE;
         final Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-        for (int x = 0  ; x < width; ++x) {
+        for (int x = 0; x < width; ++x) {
             for (int y = 0; y < height; ++y) {
-                bmp.setPixel(x, y, matrix.get(x, y) ? qrcode_color : background_color);
+                bmp.setPixel(x, y, matrix.get(x / SCALE, y / SCALE) == 1 ? qrcode_color : background_color);
             }
         }
         return bmp;
     }
 
     public QrBitmap call() throws WriterException {
-        // FIXME: change hardcoded 2048 ?
-        this.qrcode = toBitmap(new QRCodeWriter().encode(data, BarcodeFormat.QR_CODE, 2048, 2048), Color.BLACK, background_color);
+        QRCode code = Encoder.encode(data, ErrorCorrectionLevel.M);
+        this.qrcode = toBitmap(code, Color.BLACK, background_color);
         return this;
     }
 }
