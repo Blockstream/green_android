Blockstream/green_android

commit 00d4e07062e6675a0c845e6cdd1953a8021dd2c1

more lint

app/src/main/java/com/greenaddress/greenbits/GaService.java
@@ -170,7 +170,7 @@ public class GaService extends Service {
             updateBalance(0);
             for (final Object subaccount : result.subaccounts) {
                 final Map<?, ?> subaccountMap = (Map) subaccount;
-                final int pointer = ((Number) subaccountMap.get("pointer")).intValue();
+                final int pointer = ((Integer) subaccountMap.get("pointer"));
                 balanceObservables.put(pointer, new GaObservable());
                 updateBalance(pointer);
             }
@@ -292,16 +292,16 @@ public class GaService extends Service {
                     final Map<?, ?> utxo = (Map) result.get(i);
                     final String txhash = (String) utxo.get("txhash");
                     final Integer blockHeight = (Integer) utxo.get("block_height");
-                    final Integer pt_idx = ((Number) utxo.get("pt_idx")).intValue();
+                    final Integer pt_idx = ((Integer) utxo.get("pt_idx"));
                     final Sha256Hash sha256hash = new Sha256Hash(Hex.decode(txhash));
                     if (!getSharedPreferences("verified_utxo_" + receivingId, MODE_PRIVATE).getBoolean(txhash, false)) {
                         recalculateBloom = true;
-                        addToBloomFilter(blockHeight, sha256hash, pt_idx, ((Number) utxo.get("subaccount")).intValue(),
-                                ((Number) utxo.get("pointer")).intValue());
+                        addToBloomFilter(blockHeight, sha256hash, pt_idx, ((Integer) utxo.get("subaccount")),
+                                ((Integer) utxo.get("pointer")));
                     } else {
                         // already verified
-                        addToUtxo(new Sha256Hash(txhash), pt_idx, ((Number) utxo.get("subaccount")).intValue(),
-                                ((Number) utxo.get("pointer")).intValue());
+                        addToUtxo(new Sha256Hash(txhash), pt_idx, ((Integer) utxo.get("subaccount")),
+                                ((Integer) utxo.get("pointer")));
                         addUtxoToValues(new Sha256Hash(txhash));
                     }
                     newUtxos.add(new TransactionOutPoint(Network.NETWORK, pt_idx, sha256hash));
@@ -356,18 +356,16 @@ public class GaService extends Service {
                 final byte[][] hashes = new byte[unspentOutputsOutpoints.size()][];
 
                 int i = 0;
-                for (final Sha256Hash hash : unspentOutputsOutpoints.keySet()) {
-                    hashes[i++] = Utils.reverseBytes(hash.getBytes());
-                }
-
                 final BloomFilter res = new BloomFilter(size, falsePositiveRate, nTweak);
-                for (i = 0; i < hashes.length; ++i) {
-                    res.insert(hashes[i]);
+                for (final Sha256Hash hash : unspentOutputsOutpoints.keySet()) {
+                    res.insert(hashes[i++] = Utils.reverseBytes(hash.getBytes()));
                 }
 
                 // add fake entry to avoid downloading blocks when filter is empty
                 // (empty bloom filters are ignored by bitcoinj)
-                res.insert(new byte[]{(byte) 0xde, (byte) 0xad, (byte) 0xbe, (byte) 0xef});
+                if (hashes.length > 0) {
+                    res.insert(new byte[]{(byte) 0xde, (byte) 0xad, (byte) 0xbe, (byte) 0xef});
+                }
                 return res;
             }
 
@@ -772,12 +770,12 @@ public class GaService extends Service {
                         final Integer subaccount = unspentOutpointsSubaccounts.get(txOutpoint);
                         final Integer pointer = unspentOutpointsPointers.get(txOutpoint);
 
-                        futuresList.add(Futures.transform(verifySpendableBy(result.getOutput(outpoint.intValue()), subaccount, pointer), new Function<Boolean, Boolean>() {
+                        futuresList.add(Futures.transform(verifySpendableBy(result.getOutput(outpoint), subaccount, pointer), new Function<Boolean, Boolean>() {
                             @Nullable
                             @Override
                             public Boolean apply(final @Nullable Boolean input) {
-                                if (input.booleanValue()) {
-                                    final Coin coinValue = result.getOutput(outpoint.intValue()).getValue();
+                                if (input) {
+                                    final Coin coinValue = result.getOutput(outpoint).getValue();
                                     addUtxoToValues(txOutpoint, subaccount, coinValue);
                                     changedSubaccounts.add(subaccount);
                                     final SharedPreferences.Editor e = getSharedPreferences("verified_utxo_spendable_value_" + receivingId, MODE_PRIVATE).edit();
@@ -882,7 +880,7 @@ public class GaService extends Service {
                 Hex.decode(Network.depositChainCode),
                 ECKey.fromPublicOnly(Hex.decode(Network.depositPubkey)).getPubKeyPoint(),
                 null, null);
-        if (subaccount.intValue() != 0) {
+        if (subaccount != 0) {
             gaWallet = HDKeyDerivation.deriveChildKey(gaWallet, new ChildNumber(3));
         } else {
             gaWallet = HDKeyDerivation.deriveChildKey(gaWallet, new ChildNumber(1));
@@ -900,8 +898,8 @@ public class GaService extends Service {
             childNum = b1 * 256 + b2;
             gaWallet = HDKeyDerivation.deriveChildKey(gaWallet, new ChildNumber(childNum));
         }
-        if (subaccount.intValue() != 0) {
-            gaWallet = HDKeyDerivation.deriveChildKey(gaWallet, new ChildNumber(subaccount.intValue(), false));
+        if (subaccount != 0) {
+            gaWallet = HDKeyDerivation.deriveChildKey(gaWallet, new ChildNumber(subaccount, false));
         }
         gaDeterministicKeys.put(subaccount, gaWallet);
         return gaWallet;
@@ -1046,7 +1044,7 @@ public class GaService extends Service {
             final Wallet fakeWallet = new Wallet(Network.NETWORK) {
                 @Override
                 public int getLastBlockSeenHeight() {
-                    return blockHeight.intValue() - 1;
+                    return blockHeight - 1;
                 }
             };
             blockChain.addWallet(fakeWallet);
@@ -1116,7 +1114,7 @@ public class GaService extends Service {
         final AsyncFunction<Map, String> verifyAddress = new AsyncFunction<Map, String>() {
             @Override
             public ListenableFuture<String> apply(final Map input) throws Exception {
-                final int pointer = ((Number) input.get("pointer")).intValue();
+                final int pointer = ((Integer) input.get("pointer"));
                 final byte[] scriptHash = Utils.sha256hash160(Hex.decode((String) input.get("script")));
                 return Futures.transform(verifyP2SHSpendableBy(
                         ScriptBuilder.createP2SHOutputScript(scriptHash),
@@ -1124,7 +1122,7 @@ public class GaService extends Service {
                     @Nullable
                     @Override
                     public String apply(final @Nullable Boolean input) {
-                        if (input.booleanValue()) {
+                        if (input) {
                             return Address.fromP2SHHash(Network.NETWORK, scriptHash).toString();
                         } else {
                             throw new IllegalArgumentException("Address validation failed");
@@ -1357,14 +1355,14 @@ public class GaService extends Service {
                 int changeIdx;
                 if (input == null) {
                     changeIdx = -1;
-                } else if (input.get(0).booleanValue()) {
+                } else if (input.get(0)) {
                     changeIdx = 0;
-                } else if (input.get(1).booleanValue()) {
+                } else if (input.get(1)) {
                     changeIdx = 1;
                 } else {
                     throw new IllegalArgumentException("Verification: Change output missing.");
                 }
-                if (input != null && input.get(0).booleanValue() && input.get(1).booleanValue()) {
+                if (input != null && input.get(0) && input.get(1)) {
                     // Shouldn't happen really. In theory user can send money to a new change address
                     // of themselves which they've generated manually, but it's unlikely, so for
                     // simplicity we don't handle it.
@@ -1457,7 +1455,7 @@ public class GaService extends Service {
         final String[] allTwoFacSystem = getResources().getStringArray(R.array.twoFactorChoicesSystem);
         final ArrayList<String> enabledTwoFac = new ArrayList<>();
         for (int i = 0; i < allTwoFac.length; ++i) {
-            if (((Boolean) twoFacConfig.get(allTwoFacSystem[i])).booleanValue()) {
+            if (((Boolean) twoFacConfig.get(allTwoFacSystem[i]))) {
                 if (useSystemNames) {
                     enabledTwoFac.add(allTwoFacSystem[i]);
                 } else {

app/src/main/java/com/greenaddress/greenapi/WalletClient.java
@@ -428,7 +428,7 @@ public class WalletClient {
                         final ArrayList subaccounts = (ArrayList) res.get("subaccounts");
                         final int[] subaccounts_int = new int[subaccounts.size()];
                         for (int i = 0; i < subaccounts.size(); ++i) {
-                            subaccounts_int[i] = ((Number) subaccounts.get(i)).intValue();
+                            subaccounts_int[i] = ((Integer) subaccounts.get(i));
                         }
                         m_notificationHandler.onNewTransaction(Integer.valueOf(wallet_id),
                                 subaccounts_int, Long.valueOf(value), txhash);

app/src/main/java/com/greenaddress/greenbits/GreenAddressApplication.java
@@ -123,7 +123,7 @@ public class GreenAddressApplication extends MultiDexApplication {
                             if (item.getItemId() == 0) {
                                 curSubaccount = 0;
                             } else {
-                                curSubaccount = ((Number) ((Map<String, ?>) subaccounts.get(item.getItemId() - 1)).get("pointer")).intValue();
+                                curSubaccount = ((Integer) ((Map<String, ?>) subaccounts.get(item.getItemId() - 1)).get("pointer"));
                             }
                             accountChangedCallback.apply(curSubaccount);
                             return false;

app/src/main/java/com/greenaddress/greenbits/ui/BTChipHWWallet.java
@@ -90,7 +90,7 @@ public class BTChipHWWallet implements ISigningWallet {
     }
 
     private String outToPath(final Output out) {
-        if (out.getSubaccount() != null && out.getSubaccount().intValue() != 0) {
+        if (out.getSubaccount() != null && out.getSubaccount() != 0) {
             return "3'/" + out.getSubaccount() + "'/1/" + out.getPointer();
         } else {
             return "1/" + out.getPointer();
@@ -219,8 +219,8 @@ public class BTChipHWWallet implements ISigningWallet {
     private String getPath() {
         final List<String> pathStr = new LinkedList<>();
         for (final Integer i : addrn) {
-            String s = String.valueOf(i.intValue() & ~0x80000000);
-            if ((i.intValue() & 0x80000000) != 0) {
+            String s = String.valueOf(i & ~0x80000000);
+            if ((i & 0x80000000) != 0) {
                 s = s + "'";
             }
             pathStr.add(s);

app/src/main/java/com/greenaddress/greenbits/ui/MainFragment.java
@@ -93,7 +93,7 @@ public class MainFragment extends GAFragment implements Observer {
             if (((Boolean) ep.get("is_relevant"))) {
                 if (((Boolean) ep.get("is_credit"))) {
                     final boolean external_social = ep.get("social_destination") != null &&
-                            ((Number) ep.get("script_type")).intValue() != P2SH_FORTIFIED_OUT;
+                            ((Integer) ep.get("script_type")) != P2SH_FORTIFIED_OUT;
                     if (!external_social) {
                         amount += Long.valueOf((String) ep.get("value"));
                         if (!((Boolean) ep.get("is_spent"))) {
@@ -458,7 +458,7 @@ public class MainFragment extends GAFragment implements Observer {
             @Override
             public void onSuccess(@Nullable final Map<?, ?> result) {
                 final List resultList = (List) result.get("list");
-                final int curBlock = ((Number) result.get("cur_block")).intValue();
+                final int curBlock = ((Integer) result.get("cur_block"));
                 getGAService().setCurBlock(curBlock);
 
                 activity.runOnUiThread(new Runnable() {

app/src/main/java/com/greenaddress/greenbits/ui/SendFragment.java
@@ -474,7 +474,7 @@ public class SendFragment extends GAFragment {
                                                                 dialogAmount = amount;
                                                                 dialogFee = fee;
                                                             }
-                                                            if (result.requires_2factor.booleanValue() && twoFacConfig != null && ((Boolean) twoFacConfig.get("any")).booleanValue()) {
+                                                            if (result.requires_2factor && twoFacConfig != null && ((Boolean) twoFacConfig.get("any"))) {
                                                                 final List<String> enabledTwoFac =
                                                                         getGAService().getEnabledTwoFacNames(true);
                                                                 if (enabledTwoFac.size() > 1) {
@@ -615,7 +615,7 @@ public class SendFragment extends GAFragment {
                                         gaService.fireBalanceChanged(0);
                                         for (final Object subaccount : gaService.getSubaccounts()) {
                                             final Map<String, ?> subaccountMap = (Map) subaccount;
-                                            gaService.fireBalanceChanged(((Number) subaccountMap.get("pointer")).intValue());
+                                            gaService.fireBalanceChanged(((Integer) subaccountMap.get("pointer")));
                                         }
 
                                         updateBalance();

app/src/main/java/com/greenaddress/greenbits/ui/TabbedMainActivity.java
@@ -272,7 +272,7 @@ public class TabbedMainActivity extends ActionBarActivity implements ActionBar.T
                                     Futures.addCallback(getGAService().verifySpendableBy(
                                             tx.getOutputs().get(0),
                                             Integer.valueOf(0),
-                                            ((Number) result.get("out_pointer")).intValue()
+                                            ((Integer) result.get("out_pointer"))
                                     ), new FutureCallback<Boolean>() {
                                         @Override
                                         public void onSuccess(final @Nullable Boolean result) {

app/src/main/java/com/satoshilabs/trezor/Trezor.java
@@ -410,11 +410,11 @@ public class Trezor {
                             addPubkeys(TrezorType.HDNodePathType.newBuilder().
                                     setNode(curGaNode).
                                     clearAddressN().
-                                    addAddressN(Integer.valueOf(curTx.change_pointer).intValue())).
+                                    addAddressN(Integer.valueOf(curTx.change_pointer))).
                             addPubkeys(TrezorType.HDNodePathType.newBuilder().
                                     setNode(curWalletNode).
                                     clearAddressN().
-                                    addAddressN(Integer.valueOf(curTx.change_pointer).intValue())).
+                                    addAddressN(Integer.valueOf(curTx.change_pointer))).
                             setM(2));
                 } else {
                     b.setMultisig(TrezorType.MultisigRedeemScriptType.newBuilder().
@@ -422,15 +422,15 @@ public class Trezor {
                             addPubkeys(TrezorType.HDNodePathType.newBuilder().
                                     setNode(curGaNode).
                                     clearAddressN().
-                                    addAddressN(Integer.valueOf(curTx.change_pointer).intValue())).
+                                    addAddressN(Integer.valueOf(curTx.change_pointer))).
                             addPubkeys(TrezorType.HDNodePathType.newBuilder().
                                     setNode(curWalletNode).
                                     clearAddressN().
-                                    addAddressN(Integer.valueOf(curTx.change_pointer).intValue())).
+                                    addAddressN(Integer.valueOf(curTx.change_pointer))).
                             addPubkeys(TrezorType.HDNodePathType.newBuilder().
                                     setNode(curRecoveryNode).
                                     clearAddressN().
-                                    addAddressN(Integer.valueOf(curTx.change_pointer).intValue())).
+                                    addAddressN(Integer.valueOf(curTx.change_pointer))).
                             setM(2));
                 }
             } else {
@@ -604,7 +604,7 @@ public class Trezor {
             build();
 		final Script changeScript;
 		if (tx.change_pointer != null) {
-			DeterministicKey changeKey = HDKeyDerivation.deriveChildKey(gaWallet, new ChildNumber(Integer.valueOf(tx.change_pointer).intValue()));
+			DeterministicKey changeKey = HDKeyDerivation.deriveChildKey(gaWallet, new ChildNumber(Integer.valueOf(tx.change_pointer)));
 			keys.add(ECKey.fromPublicOnly(changeKey.getPubKeyPoint()));
 
 			final Integer[] intArray;

commit d97e8caa286a1c3e8c7f980c513e567fa83a75fc

fix getBloomFilterElementCount - broke in 00d4e07

app/src/main/java/com/greenaddress/greenbits/GaService.java
@@ -347,8 +347,8 @@ public class GaService extends Service {
 
             @Override
             public int getBloomFilterElementCount() {
-                // + 1 to avoid downloading full blocks (empty bloom filters are ignored by bitcoinj)
-                return unspentOutputsOutpoints.size() + 1;
+                // 1 to avoid downloading full blocks (empty bloom filters are ignored by bitcoinj)
+                return unspentOutputsOutpoints.isEmpty() ? 1: unspentOutpointsPointers.size();
             }
 
             @Override