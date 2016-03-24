7c4be567973efc109e4fb89571f43c98f8fb43fc
GreenAddress <info@greenaddress.it>
2016-03-24 11:32:51 +0100

    remove unused code

--- app/src/main/java/com/greenaddress/greenbits/ui/preferences/ProxySettingsActivity.java
+++ app/src/main/java/com/greenaddress/greenbits/ui/preferences/ProxySettingsActivity.java
@@ -6,8 +6,6 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.support.v7.app.ActionBar;
@@ -29,35 +27,6 @@ import java.util.List;
  * API Guide</a> for more information on developing a Settings UI.
  */
 public class ProxySettingsActivity extends AppCompatPreferenceActivity {
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
-    private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object value) {
-            String stringValue = value.toString();
-
-            if (preference instanceof ListPreference) {
-                // For list preferences, look up the correct display value in
-                // the preference's 'entries' list.
-                ListPreference listPreference = (ListPreference) preference;
-                int index = listPreference.findIndexOfValue(stringValue);
-
-                // Set the summary to reflect the new value.
-                preference.setSummary(
-                        index >= 0
-                                ? listPreference.getEntries()[index]
-                                : null);
-
-            }  else {
-                // For all other preferences, set the summary to the value's
-                // simple string representation.
-                preference.setSummary(stringValue);
-            }
-            return true;
-        }
-    };
 
     /**
      * Helper method to determine if the device has an extra-large screen. For
