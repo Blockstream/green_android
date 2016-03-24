4c4f17d2d5ade42cb528b76ec6832b2b0fcc46ce
GreenAddress <info@greenaddress.it>
2016-03-24 12:31:06 +0100

    remove dead code, use local default local modifier

--- app/src/main/java/com/greenaddress/greenbits/ui/preferences/AppCompatPreferenceActivity.java
+++ app/src/main/java/com/greenaddress/greenbits/ui/preferences/AppCompatPreferenceActivity.java
@@ -4,10 +4,8 @@ import android.content.res.Configuration;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.support.annotation.LayoutRes;
-import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatDelegate;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -33,12 +31,8 @@ public abstract class AppCompatPreferenceActivity extends PreferenceActivity {
         getDelegate().onPostCreate(savedInstanceState);
     }
 
-    public ActionBar getSupportActionBar() {
+    ActionBar getSupportActionBar() {
         return getDelegate().getSupportActionBar();
-    }
-
-    public void setSupportActionBar(@Nullable Toolbar toolbar) {
-        getDelegate().setSupportActionBar(toolbar);
     }
 
     @Override
