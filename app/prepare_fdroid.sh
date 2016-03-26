f9b630f58f0790258f06d373b6a2e7a4d1680d9a
GreenAddress <info@greenaddress.it>
2016-03-26 21:03:57 +0100

    use secp256k1 for signing, update F-Droid script, strip binaries

--- app/prepare_fdroid.sh
+++ app/prepare_fdroid.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+
+set -e
+
+
 cd scrypt
 patch -p1 < ../scrypt_Makefile.patch
-if [ `uname -m` = "x86_64" ]; then
+
+if [ `uname` == "Darwin" ]; then
+    export SYSTEM="darwin-x86_64"
+elif [ `uname -m` = "x86_64" ]; then
     export SYSTEM="linux-x86_64"
 else
     export SYSTEM="linux-x86"
 fi
+
 for ARCH in arm-linux-androideabi mipsel-linux-android x86; do
     export ARCH_SHORT=`echo $ARCH | cut -d'-' -f 1 | sed s/mipsel/mips/` # arm|mips|x86
     export PATH=$ANDROID_NDK/toolchains/$ARCH-4.8/prebuilt/$SYSTEM/bin:$PATH
@@ -13,5 +22,32 @@ for ARCH in arm-linux-androideabi mipsel-linux-android x86; do
     ARCHDIR=../src/main/jniLibs/`echo $ARCH_SHORT | sed s/arm/armeabi/` # armeabi|mips|x86
     mkdir -p $ARCHDIR
     cp target/libscrypt.so $ARCHDIR
+    strip $ARCHDIR/libscrypt.so
 done
+
+cd ../secp256k1
+
+./autogen.sh
+
+for ARCH in arm-linux-androideabi mipsel-linux-android x86; do
+    export PATH=$ANDROID_NDK/toolchains/$ARCH-4.8/prebuilt/$SYSTEM/bin:$PATH
+
+    export ARCH_SHORT=`echo $ARCH | cut -d'-' -f 1 | sed s/mipsel/mips/` # arm|mips|x86
+    export ARCH=`echo $ARCH | sed s/x86/i686-linux-android/`
+
+    export SYSROOT=$ANDROID_NDK/platforms/android-9/arch-$ARCH_SHORT/
+    export CC=$ARCH-gcc
+    export CPP=$ARCH-cpp
+    export CPPFLAGS=--sysroot=$SYSROOT
+    export CFLAGS=--sysroot=$SYSROOT
+
+    ./configure --host=$ARCH --target=$ARCH --enable-jni --enable-experimental --enable-module-schnorr --enable-module-ecdh
+    make -o configure clean
+    make -o configure
+    ARCHDIR=output/`echo $ARCH_SHORT | sed s/arm/armeabi/` # armeabi|mips|x86
+    mkdir -p $ARCHDIR
+    cp .libs/libsecp256k1.so $ARCHDIR
+    strip $ARCHDIR/libsecp256k1.so
+done
+
 cd ..
