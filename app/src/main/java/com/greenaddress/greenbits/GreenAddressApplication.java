8d84ee7310b22971aee8815ea9109fab0c5a73d2
GreenAddress <info@greenaddress.it>
2016-03-28 22:44:42 +0200

    randomise secp256k1 context, add strong failure mode

--- app/src/main/java/com/greenaddress/greenbits/GreenAddressApplication.java
+++ app/src/main/java/com/greenaddress/greenbits/GreenAddressApplication.java
@@ -21,9 +21,15 @@ import android.widget.TextView;
 import com.google.common.base.Function;
 import com.google.common.util.concurrent.SettableFuture;
 import com.greenaddress.greenapi.GAException;
+import com.greenaddress.greenbits.ui.FailHardActivity;
 import com.greenaddress.greenbits.ui.R;
 import com.greenaddress.greenbits.ui.preferences.SettingsActivity;
 
+import org.bitcoin.NativeSecp256k1;
+import org.bitcoin.NativeSecp256k1Util;
+import org.bitcoin.Secp256k1Context;
+
+import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.Map;
 import java.util.Observable;
@@ -34,6 +40,7 @@ public class GreenAddressApplication extends MultiDexApplication {
     public GaService gaService;
     @NonNull public final SettableFuture<Void> onServiceAttached = SettableFuture.create();
     private boolean mBound = false;
+    private String errorTitle, errorContent;
     @Nullable
     private ConnectivityObservable connectionObservable = new ConnectivityObservable();
     @Nullable
@@ -59,15 +66,61 @@ public class GreenAddressApplication extends MultiDexApplication {
 
     @Nullable
     public ConnectivityObservable getConnectionObservable() {
+        if (errorTitle != null) {
+            failHard();
+        }
         return connectionObservable;
+    }
+
+    private boolean randomizeSecp256k1Context() {
+        try {
+            final SecureRandom secureRandom = new SecureRandom();
+            final byte[] seed = new byte[32];
+            secureRandom.nextBytes(seed);
+            return NativeSecp256k1.randomize(seed);
+
+        } catch (final NativeSecp256k1Util.AssertFailException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private void failHard() {
+        // fail hard
+        if (errorTitle != null) {
+            final Intent fail = new Intent(this, FailHardActivity.class);
+            fail.putExtra("errorTitle", errorTitle);
+            fail.putExtra("errorContent", errorContent);
+            fail.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(fail);
+        }
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        PRNGFixes.apply();
-        final Intent intent = new Intent(this, GaService.class);
-        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+
+        try {
+            PRNGFixes.apply();
+        } catch (final SecurityException e) {
+            e.printStackTrace();
+            errorTitle = "Security exception";
+            errorContent = String.format("{} please contact support info@greenaddress.it", e.getMessage());
+        }
+
+        if (errorTitle == null && Secp256k1Context.isEnabled()) {
+            if (randomizeSecp256k1Context()) {
+                final Intent intent = new Intent(this, GaService.class);
+                bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+            } else {
+                errorTitle = "Randomization failed";
+                errorContent = "Warning: Randomization of secp256k1lib context failed, please contact support info@greenaddress.it";
+            }
+        } else if (errorTitle == null){
+            errorTitle = "Libsecp256k1 not found";
+            errorContent = "libsecp256k1.so not found, this platform is not supported, please contact support info@greenaddress.it";
+        }
+        failHard();
     }
 
     @Override
---
+++ app/src/main/java/com/greenaddress/greenbits/ui/FailHardActivity.java
@@ -0,0 +1,49 @@
+package com.greenaddress.greenbits.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.Theme;
+
+public class FailHardActivity extends AppCompatActivity {
+    private final static String TAG = FailHardActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fail_hard);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        final Intent i = getIntent();
+        Log.e(TAG, i.getStringExtra("errorTitle"));
+        Log.e(TAG, i.getStringExtra("errorContent"));
+
+        (new MaterialDialog.Builder(this)
+                .title(i.getStringExtra("errorTitle"))
+                .content(i.getStringExtra("errorContent"))
+                .positiveColorRes(R.color.accent)
+                .negativeColorRes(R.color.white)
+                .titleColorRes(R.color.white)
+                .contentColorRes(android.R.color.white)
+                .theme(Theme.DARK)
+                .positiveText("CLOSE")
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull final MaterialDialog dialog, @NonNull final DialogAction which) {
+                        final Intent main = new Intent(Intent.ACTION_MAIN);
+                        main.addCategory(Intent.CATEGORY_HOME);
+                        main.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(main);
+                        finish();
+                    }
+                })).build().show();
+        }
+}
