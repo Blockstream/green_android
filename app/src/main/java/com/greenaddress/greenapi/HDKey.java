10d23e7e435d65f24fa07fc6f7553c13a3949931
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-06-08 02:49:27 +1200

    Implement HDKey node methods and add temporary reconciliation code

--- app/src/main/java/com/greenaddress/greenapi/HDKey.java
+++ app/src/main/java/com/greenaddress/greenapi/HDKey.java
@@ -19,9 +19,9 @@ public class HDKey {
     private final static int VER_PUBLIC = isMain() ? BIP32_VER_MAIN_PUBLIC : BIP32_VER_TEST_PUBLIC;
     private final static int VER_PRIVATE = isMain() ? BIP32_VER_MAIN_PRIVATE : BIP32_VER_TEST_PRIVATE;
 
-    private final Object mImpl;
+    private Object mImpl;
 
-    static private boolean isMain() {
+    private static boolean isMain() {
         return NetworkParameters.fromID(NetworkParameters.ID_MAINNET).equals(Network.NETWORK);
     }
 
@@ -29,34 +29,83 @@ public class HDKey {
         mImpl = Wally.bip32_key_from_seed(seed, VER_PRIVATE);
     }
 
+    public HDKey(final byte[] chainCode, final byte[] publicKey) {
+        mImpl = Wally.bip32_key_init(VER_PUBLIC, 0, 0, chainCode, publicKey, null, null, null);
+    }
+
+    public void close() {
+        if (mImpl == null)
+            return;
+        Wally.bip32_key_free(mImpl);
+        mImpl = null;
+    }
+
+    public final byte[] getPrivateKey() { return Wally.bip32_key_get_priv_key(mImpl); }
+    public final byte[] getPublicKey() { return Wally.bip32_key_get_pub_key(mImpl); }
+    public final byte[] getChainCode() { return Wally.bip32_key_get_chain_code(mImpl); }
+    public final int getChildNumber() { return Wally.bip32_key_get_child_num(mImpl); }
+
+    public HDKey derive(final Integer childNum) {
+        return derive(childNum, getPrivateKey() != null ? VER_PRIVATE : VER_PUBLIC);
+    }
+    public HDKey derivePublic(final Integer childNum) { return derive(childNum, VER_PUBLIC); }
+    public HDKey derivePrivate(final Integer childNum) { return derive(childNum, VER_PRIVATE); }
+
+    private HDKey derive(final Integer childNum, int version) {
+        Object derived = Wally.bip32_key_from_parent(mImpl, childNum, version);
+        close();
+        mImpl = derived;
+        return this;
+    }
+
+    private static byte[] h(final String hex) { return Wally.hex_to_bytes(hex); }
+
+    //
     // Temporary methods for use while converting from DeterministicKey
-    static public DeterministicKey deriveChildKey(final DeterministicKey parent, final Integer childNum) {
+    public static DeterministicKey deriveChildKey(final DeterministicKey parent, final Integer childNum) {
         return HDKeyDerivation.deriveChildKey(parent, new ChildNumber(childNum));
     }
 
-    static public DeterministicKey createMasterKeyFromSeed(final byte[] seed) {
+    public static DeterministicKey createMasterKeyFromSeed(final byte[] seed) {
         return HDKeyDerivation.createMasterPrivateKey(seed);
     }
 
-    static public DeterministicKey createMasterKey(final byte[] chainCode, final byte[] publicKey) {
+    public static DeterministicKey createMasterKey(final byte[] chainCode, final byte[] publicKey) {
         final ECKey pub = ECKey.fromPublicOnly(publicKey);
         return new DeterministicKey(new ImmutableList.Builder<ChildNumber>().build(),
                                     chainCode, pub.getPubKeyPoint(), null, null);
     }
 
-    static public DeterministicKey createMasterKey(final String chainCode, final String publicKey) {
-        return createMasterKey(Wally.hex_to_bytes(chainCode), Wally.hex_to_bytes(publicKey));
+    public static DeterministicKey createMasterKey(final String chainCode, final String publicKey) {
+        return createMasterKey(h(chainCode), h(publicKey));
     }
 
     // Get the key derived from the servers public key/chaincode plus the users path.
     // This is the key used on the servers side of 2-of-2 transactions.
-    static public DeterministicKey getServerSubaccountKey(final int[] path, final Integer subaccount) {
+    public static DeterministicKey getServerSubaccountKey(final int[] path, final Integer subaccount) {
         DeterministicKey k = createMasterKey(Network.depositChainCode, Network.depositPubkey);
         k = deriveChildKey(k, subaccount == 0 ? 1 : 3);
         for (int i : path)
             k = deriveChildKey(k, i);
         if (subaccount != 0)
             k = deriveChildKey(k, subaccount);
+
+        // Reconcile against wally
+        HDKey hd = new HDKey(h(Network.depositChainCode), h(Network.depositPubkey));
+        hd.derivePublic(subaccount == 0 ? 1 : 3);
+        for (int i : path)
+            hd.derivePublic(i);
+        if (subaccount != 0)
+            hd.derivePublic(subaccount);
+
+        if (!h(k.getChainCode()).equals(h(hd.getChainCode())))
+            throw new RuntimeException("Chain code mismatch");
+        if (!h(k.getPubKey()).equals(h(hd.getPublicKey())))
+            throw new RuntimeException("Public key mismatch");
+
         return k;
     }
+    // FIXME: Remove
+    private static String h(final byte[] bytes) { return Wally.hex_from_bytes(bytes); }
+
 }
