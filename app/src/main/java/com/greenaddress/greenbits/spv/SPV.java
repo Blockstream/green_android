41196abaf2856421ee23b944baea4264a4a86511
Jon Griffiths <jon_p_griffiths@yahoo.com>
2016-06-21 13:51:39 +1200

    Remove unused import

--- app/src/main/java/com/greenaddress/greenbits/spv/SPV.java
+++ app/src/main/java/com/greenaddress/greenbits/spv/SPV.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.blockstream.libwally.Wally;
 import com.google.common.base.Function;
