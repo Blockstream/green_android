Blockstream/green_android

commit 80f7c1fcf083dc01f0bb480fa334ac65e54c4c95

more finalization and unnecessary brackets removal

app/src/main/java/com/greenaddress/greenbits/GaService.java
@@ -92,7 +92,7 @@ public class GaService extends Service implements INotificationHandler {
     @Override
     public IBinder onBind(final Intent intent) { return mBinder; }
 
-    public void onBound(GreenAddressApplication app) {
+    public void onBound(final GreenAddressApplication app) {
         // Update our state when network connectivity changes.
         mNetConnectivityReceiver = new BroadcastReceiver() {
             public void onReceive(final Context context, final Intent intent) {
@@ -123,7 +123,7 @@ public class GaService extends Service implements INotificationHandler {
     private float mFiatRate;
     private String mFiatCurrency;
     private String mFiatExchange;
-    private ArrayList mSubaccounts;
+    private ArrayList<Map<String, ?>> mSubaccounts;
     private String mReceivingId;
     private Map<?, ?> mTwoFactorConfig;
     private final GaObservable mTwoFactorConfigObservable = new GaObservable();
@@ -372,11 +372,10 @@ public class GaService extends Service implements INotificationHandler {
         return Futures.transform(createOutScript(subAccount, pointer), new Function<byte[], Boolean>() {
             @Override
             public Boolean apply(final byte[] multisig) {
-                if (getLoginData().segwit) {
+                if (getLoginData().segwit &&
+                        Arrays.equals(gotP2SH, Utils.sha256hash160(getSegWitScript(multisig)))) {
                     // allow segwit p2sh only if segwit is enabled
-                    if (Arrays.equals(gotP2SH, Utils.sha256hash160(getSegWitScript(multisig)))) {
-                        return true;
-                    }
+                    return true;
                 }
 
                 final byte[] expectedP2SH = Utils.sha256hash160(multisig);
@@ -445,8 +444,7 @@ public class GaService extends Service implements INotificationHandler {
 
         mBalanceObservables.put(0, new GaObservable());
         updateBalance(0);
-        for (final Object s : loginData.subAccounts) {
-            final Map<?, ?> m = (Map) s;
+        for (final Map<String, ?> m : loginData.subAccounts) {
             final int pointer = ((Integer) m.get("pointer"));
             mBalanceObservables.put(pointer, new GaObservable());
             updateBalance(pointer);
@@ -561,7 +559,7 @@ public class GaService extends Service implements INotificationHandler {
         return mExecutor.submit(new Callable<Map<?, ?>>() {
             @Override
             public Map<?, ?> call() throws Exception {
-                Map<?, ?> result = mClient.getMyTransactions(subAccount);
+                final Map<?, ?> result = mClient.getMyTransactions(subAccount);
                 setCurrentBlock((Integer) result.get("cur_block"));
                 return result;
             }
@@ -600,13 +598,11 @@ public class GaService extends Service implements INotificationHandler {
         final int subAccount = privateData.containsKey("subaccount")? (int) privateData.get("subaccount"):0;
         // skip fetching raw if not needed
         final Coin verifiedBalance = getSPVVerifiedBalance(subAccount);
-        if (!isSPVEnabled() ||
-            verifiedBalance == null || !verifiedBalance.equals(getCoinBalance(subAccount)) ||
-            mClient.getSigningWallet().requiresPrevoutRawTxs()) {
-            privateData.put("prevouts_mode", "http");
-        } else {
-            privateData.put("prevouts_mode", "skip");
-        }
+        final boolean useHttp = !isSPVEnabled() ||
+                verifiedBalance == null || !verifiedBalance.equals(getCoinBalance(subAccount)) ||
+                mClient.getSigningWallet().requiresPrevoutRawTxs();
+
+        privateData.put("prevouts_mode", useHttp ? "http" : "skip");
 
         final Object rbf_optin = getUserConfig("replace_by_fee");
         if (rbf_optin != null)
@@ -641,11 +637,11 @@ public class GaService extends Service implements INotificationHandler {
         }, mExecutor);
     }
 
-    public ListenableFuture<Map<String, Object>> sendRawTransaction(Transaction tx, Map<String, Object> twoFacData, final boolean returnErrorUri) {
+    public ListenableFuture<Map<String, Object>> sendRawTransaction(final Transaction tx, final Map<String, Object> twoFacData, final boolean returnErrorUri) {
         return mClient.sendRawTransaction(tx, twoFacData, returnErrorUri);
     }
 
-    public ListenableFuture<ArrayList> getAllUnspentOutputs(int confs, final Integer subAccount) {
+    public ListenableFuture<ArrayList> getAllUnspentOutputs(final int confs, final Integer subAccount) {
         return mClient.getAllUnspentOutputs(confs, subAccount);
     }
 
@@ -690,14 +686,13 @@ public class GaService extends Service implements INotificationHandler {
                 if (getLoginData().segwit) {
                     // allow segwit p2sh only if segwit is enabled
                     scriptHash = Utils.sha256hash160(getSegWitScript(script));
-                } else {
+                } else
                     scriptHash = Utils.sha256hash160(script);
-                }
 
                 final ListenableFuture<Boolean> verify;
-                if (isWatchOnly()) {
+                if (isWatchOnly())
                     verify = Futures.immediateFuture(true);
-                } else {
+                else {
                     final Script sc;
                     sc = ScriptBuilder.createP2SHOutputScript(scriptHash);
                     verify = verifyP2SHSpendableBy(sc, subAccount, pointer);
@@ -819,7 +814,7 @@ public class GaService extends Service implements INotificationHandler {
         return mFiatExchange;
     }
 
-    public ArrayList getSubaccounts() {
+    public ArrayList<Map<String, ?>> getSubaccounts() {
         return mSubaccounts;
     }
 
@@ -828,14 +823,11 @@ public class GaService extends Service implements INotificationHandler {
     }
 
     public Map<String, ?> findSubaccount(final String type, final Integer subAccount) {
-        if (haveSubaccounts()) {
-            for (final Object s : mSubaccounts) {
-                final Map<String, ?> ret = (Map) s;
+        if (haveSubaccounts())
+            for (final Map<String, ?> ret : mSubaccounts)
                 if (ret.get("pointer").equals(subAccount) &&
                    (type == null || ret.get("type").equals(type)))
                     return ret;
-            }
-        }
         return null;
     }
 
@@ -899,13 +891,12 @@ public class GaService extends Service implements INotificationHandler {
         final String[] allTwoFac = getResources().getStringArray(R.array.twoFactorChoices);
         final String[] allTwoFacSystem = getResources().getStringArray(R.array.twoFactorChoicesSystem);
         final ArrayList<String> enabledTwoFac = new ArrayList<>();
-        for (int i = 0; i < allTwoFac.length; ++i) {
-            if (((Boolean) mTwoFactorConfig.get(allTwoFacSystem[i]))) {
-                if (useSystemNames) {
-                    enabledTwoFac.add(allTwoFacSystem[i]);
-                } else {
-                    enabledTwoFac.add(allTwoFac[i]);
-                }
+        final String[] twoFacs = useSystemNames ? allTwoFacSystem : allTwoFac;
+
+        for (int i = 0; i < twoFacs.length; ++i) {
+            final String twoFac = twoFacs[i];
+            if (((Boolean) mTwoFactorConfig.get(twoFac))) {
+                enabledTwoFac.add(twoFac);
             }
         }
         return enabledTwoFac;
@@ -1070,5 +1061,9 @@ public class GaService extends Service implements INotificationHandler {
         cm = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
         final NetworkInfo ni = cm.getActiveNetworkInfo();
         return ni != null && ni.isConnectedOrConnecting() ? ni : null;
+    }
+
+    public static Transaction buildTransaction(final String hex) {
+        return new Transaction(Network.NETWORK, Wally.hex_to_bytes(hex));
     }
 }

app/src/main/java/com/greenaddress/greenapi/LoginData.java
@@ -14,7 +14,7 @@ public class LoginData {
     public String currency;
     public final Map<String, Object> userConfig;
     public Map<String, Object> feeEstimates;
-    public final ArrayList subAccounts;
+    public final ArrayList<Map<String, ?>> subAccounts;
     public final String receivingId;
     public int[] gaUserPath;  // can change on first login (registration)
     public final int earliest_key_creation_time;

app/src/main/java/com/greenaddress/greenapi/PreparedTransaction.java
@@ -3,6 +3,7 @@ package com.greenaddress.greenapi;
 import android.webkit.URLUtil;
 
 import com.blockstream.libwally.Wally;
+import com.greenaddress.greenbits.GaService;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 
@@ -45,7 +46,7 @@ public class PreparedTransaction {
 
         public PreparedData(final Map<?, ?> values,
                             final Map<String, ?> privateData,
-                            final ArrayList subAccounts,
+                            final ArrayList<Map<String, ?>> subAccounts,
                             final OkHttpClient client)
 
         {
@@ -57,9 +58,8 @@ public class PreparedTransaction {
         }
         final Map<?, ?> mValues;
         final Map<String, ?> mPrivateData;
-        final ArrayList mSubAccounts;
+        final ArrayList<Map<String, ?>> mSubAccounts;
         final OkHttpClient mClient;
-
     }
 
     public PreparedTransaction(final PreparedData pte) {
@@ -73,14 +73,12 @@ public class PreparedTransaction {
             byte[] chaincode = null, pubkey = null;
             if (mSubAccount != 0) {
                 // Check if the sub-account is 2of3 and if so store its chaincode/public key
-                for (final Object s : pte.mSubAccounts) {
-                    final Map<String, ?> m = (Map) s;
+                for (final Map<String, ?> m : pte.mSubAccounts)
                     if (m.get("type").equals("2of3") && m.get("pointer").equals(mSubAccount)) {
                         chaincode = getBytes(m, "2of3_backup_chaincode");
                         pubkey = getBytes(m, "2of3_backup_pubkey");
                         break;
                     }
-                }
             }
             mTwoOfThreeBackupChaincode = chaincode;
             mTwoOfThreeBackupPubkey = pubkey;
@@ -95,13 +93,12 @@ public class PreparedTransaction {
             mChangePointer = null;
 
         mRequiresTwoFactor = (Boolean) pte.mValues.get("requires_2factor");
-        mDecoded = new Transaction(Network.NETWORK, Wally.hex_to_bytes(pte.mValues.get("tx").toString()));
+        mDecoded = GaService.buildTransaction((String) pte.mValues.get("tx"));
 
         // return early if no rawtxs url is given, assumes user asked for 'skip'
         try {
-            if (!URLUtil.isValidUrl((String) pte.mValues.get("prevout_rawtxs"))) {
+            if (!URLUtil.isValidUrl((String) pte.mValues.get("prevout_rawtxs")))
                 return;
-            }
         } catch (final Exception e) {
             return;
         }
@@ -118,7 +115,7 @@ public class PreparedTransaction {
 
             while (keys.hasNext()) {
                 final String k = (String)keys.next();
-                mPrevoutRawTxs.put(k, new Transaction(Network.NETWORK, Wally.hex_to_bytes(prevout_rawtxs.getString(k))));
+                mPrevoutRawTxs.put(k, GaService.buildTransaction(prevout_rawtxs.getString(k)));
             }
 
         } catch (final IOException | JSONException e) {

app/src/main/java/com/greenaddress/greenapi/WalletClient.java
@@ -15,6 +15,7 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListeningExecutorService;
 import com.google.common.util.concurrent.SettableFuture;
+import com.greenaddress.greenbits.GaService;
 import com.greenaddress.greenbits.spv.Socks5SocketFactory;
 import com.greenaddress.greenbits.ui.BuildConfig;
 import com.squareup.okhttp.OkHttpClient;
@@ -153,7 +154,7 @@ public class WalletClient {
     private SettableFuture clientCall(final SettableFuture rpc,
                                       final String procedure, final Class result,
                                       final CallHandler handler, final ErrorHandler errHandler,
-                                      Object... args) {
+                                      final Object... args) {
         final ObjectMapper mapper = new ObjectMapper();
         final ArrayNode argsNode = mapper.valueToTree(Arrays.asList(args));
 
@@ -198,11 +199,11 @@ public class WalletClient {
 
     private SettableFuture clientCall(final SettableFuture rpc,
                                       final String procedure, final Class result,
-                                      final CallHandler handler, Object... args) {
+                                      final CallHandler handler, final Object... args) {
         return clientCall(rpc, procedure, result, handler, null, args);
     }
 
-    private <V> ListenableFuture<V> simpleCall(final String procedure, final Class result, Object... args) {
+    private <V> ListenableFuture<V> simpleCall(final String procedure, final Class result, final Object... args) {
         final SettableFuture<V> rpc = SettableFuture.create();
         final CallHandler handler = result == null ? stringHandler(rpc) : simpleHandler(rpc);
         final Class resultClass = result == null ? String.class : result;
@@ -210,7 +211,7 @@ public class WalletClient {
     }
 
     private <T> T syncCall(final String procedure, final Class result,
-                           Object... args) throws Exception {
+                           final Object... args) throws Exception {
 
         if (mConnection == null)
             throw new GAException("not connected");
@@ -266,17 +267,17 @@ public class WalletClient {
     }
 
     public void setProxy(final String host, final String port) {
-        if (!TextUtils.isEmpty(host) && !TextUtils.isEmpty(port)) {
-            try {
-                mProxyAddress = new InetSocketAddress(host, Integer.parseInt(port));
-                mHttpClient.setSocketFactory(new Socks5SocketFactory(host, port));
-            } catch (final UnknownHostException e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        } else {
+        if (TextUtils.isEmpty(host) || TextUtils.isEmpty(port)) {
             mProxyAddress = null;
             mHttpClient.setSocketFactory(null);
+            return;
+        }
+        try {
+            mProxyAddress = new InetSocketAddress(host, Integer.parseInt(port));
+            mHttpClient.setSocketFactory(new Socks5SocketFactory(host, port));
+        } catch (final UnknownHostException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
 
@@ -486,15 +487,18 @@ public class WalletClient {
                                 // and the session was established
                                 connected = true;
                                 rpc.set(null);
-                            } else if (newStatus instanceof WampClient.DisconnectedState) {
-                                if (!initialDisconnectedStateSeen) {
+                                return;
+                            }
+                            
+                            if (newStatus instanceof WampClient.DisconnectedState)
+                                if (!initialDisconnectedStateSeen)
                                     // First state set is always 'disconnected'
                                     initialDisconnectedStateSeen = true;
-                                } else {
-                                    if (connected) {
+                                else
+                                    if (connected)
                                         // Client got disconnected from the remote router
                                         mNotificationHandler.onConnectionClosed(0);
-                                    } else {
+                                    else {
                                         // or the last possible connect attempt failed
                                         final Throwable t = ((WampClient.DisconnectedState) newStatus).disconnectReason();
                                         if (t != null)
@@ -502,8 +506,6 @@ public class WalletClient {
                                         else
                                             rpc.setException(new GAException("Disconnected"));
                                     }
-                                }
-                            }
                         }
                     }, new Action1<Throwable>() {
                         @Override
@@ -535,9 +537,8 @@ public class WalletClient {
                     @Override
                     public void onEvent(final String topicUri, final Object event) {
                         Log.i(TAG, "FEE_ESTIMATES IS " + event.toString());
-                        if (mLoginData != null) {
+                        if (mLoginData != null)
                             mLoginData.feeEstimates = (Map) event;
-                        }
                     }
                 });
             }
@@ -547,7 +548,6 @@ public class WalletClient {
                 t.printStackTrace();
             }
         }, mExecutor);
-
 
         return rpc;
     }
@@ -732,7 +732,7 @@ public class WalletClient {
         return simpleCall("vault.send_tx", null, args, TfaData);
     }
 
-    public ListenableFuture<Map<String, Object>> sendRawTransaction(Transaction tx, Map<String, Object> twoFacData, final boolean returnErrorUri) {
+    public ListenableFuture<Map<String, Object>> sendRawTransaction(final Transaction tx, final Map<String, Object> twoFacData, final boolean returnErrorUri) {
         final SettableFuture<Map<String, Object>> rpc = SettableFuture.create();
         final ErrorHandler errHandler = new ErrorHandler() {
             public void onError(final String uri, final String err) {
@@ -756,8 +756,8 @@ public class WalletClient {
         return mLoginData.userConfig.get(key);
     }
 
-    private <T> ByteArrayOutputStream serializeJSON(T src) throws GAException {
-        ByteArrayOutputStream b = new ByteArrayOutputStream();
+    private <T> ByteArrayOutputStream serializeJSON(final T src) throws GAException {
+        final ByteArrayOutputStream b = new ByteArrayOutputStream();
         try {
             new MappingJsonFactory().getCodec().writeValue(b, src);
         } catch (final IOException e) {
@@ -813,15 +813,15 @@ public class WalletClient {
         return mHDParent;
     }
 
-    public ListenableFuture<ArrayList> getAllUnspentOutputs(int confs, Integer subAccount) {
+    public ListenableFuture<ArrayList> getAllUnspentOutputs(final int confs, final Integer subAccount) {
         return simpleCall("txs.get_all_unspent_outputs", ArrayList.class, confs, subAccount);
     }
 
-    private ListenableFuture<Transaction> transactionCall(final String procedure, Object... args) {
+    private ListenableFuture<Transaction> transactionCall(final String procedure, final Object... args) {
         final SettableFuture<Transaction> rpc = SettableFuture.create();
         final CallHandler handler = new CallHandler() {
             public void onResult(final Object tx) {
-                rpc.set(new Transaction(Network.NETWORK, Wally.hex_to_bytes((String) tx)));
+                rpc.set(GaService.buildTransaction((String) tx));
             }
         };
         return clientCall(rpc, procedure, String.class, handler, args);

app/src/main/java/com/greenaddress/greenbits/ui/TabbedMainActivity.java
@@ -230,19 +230,15 @@ public class TabbedMainActivity extends GaActivity implements Observer {
                 final Parcelable[] rawMessages = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
                 for (Parcelable ndefMsg_ : rawMessages) {
                     final NdefMessage ndefMsg = (NdefMessage) ndefMsg_;
-                    for (NdefRecord record : ndefMsg.getRecords()) {
-                        if (record.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(record.getType(), NdefRecord.RTD_URI)) {
+                    for (NdefRecord record : ndefMsg.getRecords())
+                        if (record.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(record.getType(), NdefRecord.RTD_URI))
                             mViewPager.setTag(R.id.tag_bitcoin_uri, record.toUri());
-                        }
-                    }
                 }
-            } else {
+            } else
                 mViewPager.setTag(R.id.tag_bitcoin_uri, getIntent().getData());
-            }
             mViewPager.setCurrentItem(2);
-        } else {
+        } else
             mViewPager.setCurrentItem(1);
-        }
     }
 
     @Override
@@ -286,9 +282,8 @@ public class TabbedMainActivity extends GaActivity implements Observer {
             case REQUEST_SEND_QR_SCAN:
                 if (data != null && data.getStringExtra(ScanActivity.INTENT_EXTRA_RESULT) != null) {
                     String scanned = data.getStringExtra(ScanActivity.INTENT_EXTRA_RESULT);
-                    if (!(scanned.length() >= 8 && scanned.substring(0, 8).equalsIgnoreCase("bitcoin:"))) {
+                    if (!(scanned.length() >= 8 && scanned.substring(0, 8).equalsIgnoreCase("bitcoin:")))
                         scanned = String.format("bitcoin:%s", scanned);
-                    }
                     final Intent browsable = new Intent(this, TabbedMainActivity.class);
                     browsable.setData(Uri.parse(scanned));
                     browsable.addCategory(Intent.CATEGORY_BROWSABLE);
@@ -296,16 +291,14 @@ public class TabbedMainActivity extends GaActivity implements Observer {
                 }
                 break;
             case REQUEST_BITCOIN_URL_LOGIN:
-                if (resultCode == RESULT_OK) {
+                if (resultCode == RESULT_OK)
                     launch(true);
-                } else {
+                else
                     finish();
-                }
                 break;
             case REQUEST_SWEEP_PRIVKEY:
-                if (data == null) {
+                if (data == null)
                     return;
-                }
                 ECKey keyNonFinal = null;
                 final String qrText = data.getStringExtra(ScanActivity.INTENT_EXTRA_RESULT);
                 try {
@@ -337,9 +330,8 @@ public class TabbedMainActivity extends GaActivity implements Observer {
                             final MonetaryFormat format;
                             format = CurrencyMapper.mapBtcUnitToFormat( (String) mService.getUserConfig("unit"));
                             Coin outputsValue = Coin.ZERO;
-                            for (final TransactionOutput output : txNonBip38.getOutputs()) {
+                            for (final TransactionOutput output : txNonBip38.getOutputs())
                                 outputsValue = outputsValue.add(output.getValue());
-                            }
                             mainText.setText(Html.fromHtml("Are you sure you want to sweep <b>all</b> ("
                                     + format.postfixCode().format(outputsValue) + ") funds from the address below?"));
                             address = keyNonBip38.toAddress(Network.NETWORK).toString();
@@ -413,17 +405,16 @@ public class TabbedMainActivity extends GaActivity implements Observer {
                             }).build().show();
                     }
                 };
-                if (keyNonBip38 != null) {
+                if (keyNonBip38 != null)
                     CB.after(mService.prepareSweepSocial(keyNonBip38.getPubKey(), false), callback);
-                } else {
+                else
                     callback.onSuccess(null);
-                }
                 break;
         }
     }
 
     private Transaction getSweepTx(final Map<?, ?> sweepResult) {
-        return new Transaction(Network.NETWORK, Wally.hex_to_bytes((String) sweepResult.get("tx")));
+        return GaService.buildTransaction((String) sweepResult.get("tx"));
     }
 
     @Override
@@ -449,11 +440,10 @@ public class TabbedMainActivity extends GaActivity implements Observer {
                 //New Marshmallow permissions paradigm
                 final String[] perms = {"android.permission.CAMERA"};
                 if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1 &&
-                        checkSelfPermission(perms[0]) != PackageManager.PERMISSION_GRANTED) {
+                        checkSelfPermission(perms[0]) != PackageManager.PERMISSION_GRANTED)
                     requestPermissions(perms, /*permsRequestCode*/ 200);
-                } else {
+                else
                     startActivityForResult(scanner, REQUEST_SWEEP_PRIVKEY);
-                }
                 return true;
             case R.id.network_unavailable:
                 return true;
@@ -474,11 +464,10 @@ public class TabbedMainActivity extends GaActivity implements Observer {
 
     @Override
     public void onBackPressed() {
-        if (mViewPager.getCurrentItem() == 1) {
+        if (mViewPager.getCurrentItem() == 1)
             finish();
-        } else {
+        else
             mViewPager.setCurrentItem(1);
-        }
     }
 
     @Override

commit 5f6239204a716d556053591459ed5c7cf6df912c

fix 2fa bug introduced yesterday in 80f7c1fcf083dc01f0bb480fa334ac65e54c4c95

app/src/main/java/com/greenaddress/greenbits/GaService.java
@@ -892,13 +892,9 @@ public class GaService extends Service implements INotificationHandler {
         final String[] allTwoFacSystem = getResources().getStringArray(R.array.twoFactorChoicesSystem);
         final ArrayList<String> enabledTwoFac = new ArrayList<>();
         final String[] twoFacs = useSystemNames ? allTwoFacSystem : allTwoFac;
-
-        for (int i = 0; i < twoFacs.length; ++i) {
-            final String twoFac = twoFacs[i];
-            if (((Boolean) mTwoFactorConfig.get(twoFac))) {
-                enabledTwoFac.add(twoFac);
-            }
-        }
+        for (int i = 0; i < allTwoFacSystem.length; ++i)
+            if (((Boolean) mTwoFactorConfig.get(allTwoFacSystem[i])))
+                enabledTwoFac.add(twoFacs[i]);
         return enabledTwoFac;
     }
 