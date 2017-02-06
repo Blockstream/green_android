fa8d090dc0ba224a0e6ed45404c78177dd5a7ea5
Jon Griffiths <jon_p_griffiths@yahoo.com>
2017-02-06 13:55:07 +1300

    Cleanup: Remove unused input

--- app/src/main/java/com/greenaddress/greenbits/ui/ReceiveFragment.java
+++ app/src/main/java/com/greenaddress/greenbits/ui/ReceiveFragment.java
@@ -21,7 +21,6 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.common.util.concurrent.FutureCallback;
