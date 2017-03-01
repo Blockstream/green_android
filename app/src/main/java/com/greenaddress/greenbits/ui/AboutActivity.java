cfac646896ff3b549279bfc62702f33d161c3cf4
Lawrence Nahum <lawrence@greenaddress.it>
2017-03-01 22:17:28 +0100

    upgrade libraries android-about-page and material-dialogs to 1.2 and 0.9.3.0 respectively

--- app/src/main/java/com/greenaddress/greenbits/ui/AboutActivity.java
+++ app/src/main/java/com/greenaddress/greenbits/ui/AboutActivity.java
@@ -3,7 +3,6 @@ package com.greenaddress.greenbits.ui;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 
 import mehdi.sakout.aboutpage.AboutPage;
@@ -14,8 +13,8 @@ public class AboutActivity extends AppCompatActivity {
     private Element getGitHubElement() {
         final Element gitHubElement = new Element();
         gitHubElement.setTitle(getString(mehdi.sakout.aboutpage.R.string.about_github));
-        gitHubElement.setIcon(mehdi.sakout.aboutpage.R.drawable.about_icon_github);
-        gitHubElement.setColor(ContextCompat.getColor(this, mehdi.sakout.aboutpage.R.color.about_github_color));
+        gitHubElement.setIconDrawable(mehdi.sakout.aboutpage.R.drawable.about_icon_github);
+        gitHubElement.setIconTint(mehdi.sakout.aboutpage.R.color.about_github_color);
         final String release = "https://github.com/greenaddress/GreenBits/releases/tag/r%s";
         final Intent intent = new Intent(Intent.ACTION_VIEW,
                 Uri.parse(String.format(release, BuildConfig.VERSION_NAME)));
