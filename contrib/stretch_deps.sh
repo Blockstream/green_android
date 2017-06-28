2b52a6cbba7c40fac2247d5405e15ef7aef315b9
Lawrence Nahum <lawrence@greenaddress.it>
2017-06-28 16:39:08 +0200

    update debian from jessie to stretch

--- contrib/stretch_deps.sh
+++ contrib/stretch_deps.sh
@@ -4,16 +4,10 @@ set -e
 export SDK_FILENAME=sdk-tools-linux-3859397.zip
 export NDK_FILENAME=android-ndk-r14b-linux-x86_64.zip
 
-echo "deb http://http.debian.net/debian jessie-backports main" | tee -a /etc/apt/sources.list
-mkdir -p ~/.gradle/
-touch ~/.gradle/gradle.properties
-echo "org.gradle.daemon=true" >> ~/.gradle/gradle.properties
-
 dpkg --add-architecture i386
 sed -i 's/deb.debian.org/httpredir.debian.org/g' /etc/apt/sources.list
 apt-get -yqq update && apt-get -yqq upgrade
-apt-get -yqq install -t jessie-backports openjdk-8-jdk ca-certificates-java
-apt-get -yqq install unzip curl make swig autoconf libtool pkg-config libc6:i386 libc6-dev:i386 libncurses5:i386 libstdc++6:i386 lib32z1 python
+apt-get -yqq install openjdk-8-jdk ca-certificates-java unzip curl make swig autoconf libtool pkg-config libc6:i386 libc6-dev:i386 libncurses5:i386 libstdc++6:i386 lib32z1 python
 update-java-alternatives -s java-1.8.0-openjdk-amd64
 
 cd /opt && curl -sSO https://dl.google.com/android/repository/${SDK_FILENAME} && unzip -qq ${SDK_FILENAME} && rm ${SDK_FILENAME}
