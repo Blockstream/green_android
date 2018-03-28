def1401e8ae4a72d49f9c97a3552ef7381f2d2bf
Lawrence Nahum <lawrence@greenaddress.it>
2018-03-28 23:07:55 +0200

    update minimum version for trezor

--- app/src/main/java/com/greenaddress/greenbits/ui/RequestLoginActivity.java
+++ app/src/main/java/com/greenaddress/greenbits/ui/RequestLoginActivity.java
@@ -151,8 +151,8 @@ public class RequestLoginActivity extends LoginActivity implements OnDiscoveredT
         boolean isFirmwareOutdated = false;
         if (t.getVendorId() == VENDOR_TREZOR) {
             isFirmwareOutdated = version.get(0) < 1 ||
-                                 (version.get(0) == 1 && version.get(1) < 5) ||
-                                 (version.get(0) == 1 && version.get(1) == 5 && version.get(2) < 2);
+                                 (version.get(0) == 1 && version.get(1) < 6) ||
+                                 (version.get(0) == 1 && version.get(1) == 6 && version.get(2) < 0);
         }
 
         if (isFirmwareOutdated) {
