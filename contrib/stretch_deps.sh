5eb07c2168ce0daa7cd3c8f916619ca38e0ca348
Lawrence Nahum <lawrence@greenaddress.it>
2019-01-21 13:14:35 +0100

    update docker image to latest debian & android platoform 28, travis@osx sdk-tools

--- contrib/stretch_deps.sh
+++ contrib/stretch_deps.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-export SDK_FILENAME=sdk-tools-linux-3859397.zip
+export SDK_FILENAME=sdk-tools-linux-4333796.zip
 
-sed -i 's/deb.debian.org/httpredir.debian.org/g' /etc/apt/sources.list
 apt-get -qq update && apt-get -yqq --no-install-recommends upgrade
 apt-get -yqq --no-install-recommends install openjdk-8-jdk ca-certificates-java unzip curl gzip perl
 update-java-alternatives -s java-1.8.0-openjdk-amd64
@@ -13,7 +12,7 @@ cd /opt && curl -sSO https://dl.google.com/android/repository/${SDK_FILENAME} &&
 #FIXME: avoid installing emulator
 yes | /opt/tools/bin/sdkmanager "tools" "platform-tools"
 yes | /opt/tools/bin/sdkmanager "build-tools;28.0.3"
-yes | /opt/tools/bin/sdkmanager "platforms;android-27"
+yes | /opt/tools/bin/sdkmanager "platforms;android-28"
 yes | /opt/tools/bin/sdkmanager "extras;android;m2repository" "extras;google;m2repository"
 apt-get -yqq autoremove && apt-get -yqq clean
 rm -rf /var/lib/apt/lists/* /var/cache/* /tmp/*
