d77d465c2188ad5e5eacef99d02a708b32eaf249
Leonardo Comandini <leo@leocomandini.org>
2019-03-29 18:37:55 +0100

    Ask user to confirm passphrase with Trezor
    If a user enters an incorrect bip39 passphrase the user will
    likely see an empty balance. Asking to enter the passphrase twice
    mitigates this situation.

--- app/src/main/java/com/greenaddress/greenbits/ui/TrezorPassphraseActivity.java
+++ app/src/main/java/com/greenaddress/greenbits/ui/TrezorPassphraseActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class TrezorPassphraseActivity extends GaActivity {
 
@@ -15,13 +16,18 @@ public class TrezorPassphraseActivity extends GaActivity {
         setTitleBackTransparent();
 
         final TextView value = UI.find(this, R.id.trezorPassphraseValue);
+        final TextView valueConfirm = UI.find(this, R.id.trezorPassphraseValueConfirm);
 
         final Button ok = UI.find(this, R.id.trezorPassphraseOk);
         ok.setOnClickListener(view -> {
-            Intent intent = getIntent();
-            intent.putExtra( String.valueOf(GaActivity.HARDWARE_PASSPHRASE_REQUEST), value.getText().toString());
-            setResult(Activity.RESULT_OK, intent);
-            finishOnUiThread();
+            if (!value.getText().toString().equals(valueConfirm.getText().toString())) {
+                UI.toast(this, R.string.id_error_passphrases_do_not_match, Toast.LENGTH_LONG);
+            } else {
+                Intent intent = getIntent();
+                intent.putExtra(String.valueOf(GaActivity.HARDWARE_PASSPHRASE_REQUEST), value.getText().toString());
+                setResult(Activity.RESULT_OK, intent);
+                finishOnUiThread();
+            }
         });
 
         final Button cancel = UI.find(this, R.id.trezorPassphraseCancel);
