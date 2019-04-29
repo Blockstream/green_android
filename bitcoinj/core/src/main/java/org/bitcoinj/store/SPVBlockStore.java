f75fcda9297bf3c9b13574c9c71b71d09ffcc6e4
Lawrence Nahum <lawrence@greenaddress.it>
2019-04-29 18:33:50 +0200

    remove unused bitcoinj code for windows that is deprecated

--- bitcoinj/core/src/main/java/org/bitcoinj/store/SPVBlockStore.java
+++ bitcoinj/core/src/main/java/org/bitcoinj/store/SPVBlockStore.java
@@ -278,10 +278,6 @@ public class SPVBlockStore implements BlockStore {
     public void close() throws BlockStoreException {
         try {
             buffer.force();
-            if (System.getProperty("os.name").toLowerCase().contains("win")) {
-                log.info("Windows mmap hack: Forcing buffer cleaning");
-                WindowsMMapHack.forceRelease(buffer);
-            }
             buffer = null;  // Allow it to be GCd and the underlying file mapping to go away.
             randomAccessFile.close();
         } catch (IOException e) {
