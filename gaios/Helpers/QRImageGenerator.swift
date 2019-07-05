8e5aa636e34138222b667e64e8ad5e70dfaf507a
Luca Vaccaro <luck87@gmail.com>
2019-07-05 12:56:44 +0200

    Improve quality of generated qrcode

--- gaios/Helpers/QRImageGenerator.swift
+++ gaios/Helpers/QRImageGenerator.swift
@@ -7,20 +7,34 @@ class QRImageGenerator {
         let data = text.data(using: String.Encoding.ascii, allowLossyConversion: false)
 
         let filter = CIFilter(name: "CIQRCodeGenerator")
-        guard let colorFilter = CIFilter(name: "CIFalseColor") else { return nil }
         filter!.setValue(data, forKey: "inputMessage")
         filter!.setValue("Q", forKey: "inputCorrectionLevel")
-        colorFilter.setValue(filter!.outputImage, forKey: "inputImage")
-        colorFilter.setValue(CIColor(color: UIColor.white), forKey: "inputColor1") // Background white
-        colorFilter.setValue(CIColor(color: UIColor.black), forKey: "inputColor0")
 
-        guard let qrCodeImage = colorFilter.outputImage else {
-            return nil
-        }
-        let scaleX = frame.size.width / qrCodeImage.extent.size.width
-        let scaleY = frame.size.height / qrCodeImage.extent.size.height
-        let scaledImage = qrCodeImage.transformed(by: CGAffineTransform(scaleX: scaleX, y: scaleY))
+        let colorFilter = CIFilter(name: "CIFalseColor")
+        colorFilter!.setValue(filter!.outputImage, forKey: "inputImage")
+        colorFilter!.setValue(CIColor(color: UIColor.white), forKey: "inputColor1")
+        colorFilter!.setValue(CIColor(color: UIColor.black), forKey: "inputColor0")
 
-        return UIImage(ciImage: scaledImage)
+        guard let qrCodeImage = colorFilter?.outputImage else { return nil }
+
+        // calculate resize radios
+        let size = qrCodeImage.extent.size
+        let widthRatio = frame.size.width / size.width
+        let heightRatio = frame.size.height / size.height
+
+        // create image from qrcode
+        guard let cgImage = CIContext().createCGImage(qrCodeImage, from: qrCodeImage.extent) else { return nil }
+        let dimension = CGSize(width: size.width * widthRatio, height: size.height * heightRatio)
+
+        // transform image without interpolation
+        UIGraphicsBeginImageContextWithOptions(dimension, true, 0)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        context.interpolationQuality = .none
+        context.translateBy(x: 0, y: dimension.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.draw(cgImage, in: context.boundingBoxOfClipPath)
+        let result = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return result
     }
 }
