60eae34820f2b4595ad4174447f1977fee0ee227
Luca Vaccaro <luck87@gmail.com>
2019-10-14 19:54:49 +0200

    Fix proxy & tor settings on network selection

--- gaios/NetworkSelection/NetworkSelectionSettings.swift
+++ gaios/NetworkSelection/NetworkSelectionSettings.swift
@@ -50,28 +50,21 @@ class NetworkSelectionSettings: UIViewController {
         tableView.keyboardDismissMode = UIScrollView.KeyboardDismissMode.onDrag
         guard let content = tableView.tableFooterView as? NetworkSelectionSettingsView else { return }
         let attributes = [NSAttributedString.Key.foregroundColor: UIColor.customTitaniumLight()]
-        content.socks5Hostname.attributedPlaceholder = NSAttributedString(string: "Socks5 Hostname",
-                                                                          attributes: attributes)
-        content.socks5Port.attributedPlaceholder = NSAttributedString(string: "Socks5 Port",
-                                                                     attributes: attributes)
+        content.socks5Hostname.attributedPlaceholder = NSAttributedString(string: NSLocalizedString("id_socks5_hostname", comment: ""), attributes: attributes)
+        content.socks5Port.attributedPlaceholder = NSAttributedString(string: NSLocalizedString("id_socks5_port", comment: ""), attributes: attributes)
         content.proxyLabel.text = NSLocalizedString("id_connect_through_a_proxy", comment: "")
         content.proxySettingsLabel.text = NSLocalizedString("id_proxy_settings", comment: "")
-        content.socks5Hostname.text = NSLocalizedString("id_socks5_hostname", comment: "")
-        content.socks5Port.text = NSLocalizedString("id_socks5_port", comment: "")
         content.torLabel.text = NSLocalizedString("id_connect_with_tor", comment: "")
         content.saveButton.setTitle(NSLocalizedString("id_save", comment: ""), for: .normal)
         content.saveButton.addTarget(self, action: #selector(saveAndDismiss), for: .touchUpInside)
         content.proxySwitch.addTarget(self, action: #selector(changeProxy), for: .valueChanged)
-        content.socks5Hostname.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: content.socks5Hostname.frame.height))
-        content.socks5Port.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: content.socks5Port.frame.height))
-        content.socks5Hostname.leftViewMode = .always
-        content.socks5Port.leftViewMode = .always
         let defaults = getUserNetworkSettings()
-        content.proxySettings.isHidden = !(defaults["proxy"] as? Bool ?? false)
         content.proxySwitch.isOn = defaults["proxy"] as? Bool ?? false
         content.socks5Hostname.text = defaults["socks5_hostname"] as? String ?? ""
         content.socks5Port.text = defaults["socks5_port"] as? String ?? ""
         content.torSwitch.isOn = defaults["tor"] as? Bool ?? false
+        content.socks5Hostname.isEnabled = content.proxySwitch.isOn
+        content.socks5Port.isEnabled = content.proxySwitch.isOn
     }
 
     func configureCancelButton() {
@@ -83,7 +76,14 @@ class NetworkSelectionSettings: UIViewController {
     }
 
     override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         view.roundCorners([.topRight, .topLeft], radius: 12)
+        guard let footerView = tableView.tableFooterView else { return }
+        let height = footerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+        if height != footerView.frame.size.height {
+            footerView.frame.size.height = height
+            tableView.tableFooterView = footerView
+        }
     }
 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -93,7 +93,8 @@ class NetworkSelectionSettings: UIViewController {
 
     @objc func changeProxy(_ sender: UISwitch) {
         guard let content = tableView.tableFooterView as? NetworkSelectionSettingsView else { return }
-        content.proxySettings.isHidden = !sender.isOn
+        content.socks5Hostname.isEnabled = content.proxySwitch.isOn
+        content.socks5Port.isEnabled = content.proxySwitch.isOn
     }
 
     @objc func dismissModal() {
@@ -109,23 +110,18 @@ class NetworkSelectionSettings: UIViewController {
             guard let content = tableView.tableFooterView as? NetworkSelectionSettingsView else { return }
             let socks5Hostname = content.socks5Hostname.text ?? ""
             let socks5Port = content.socks5Port.text ?? ""
-            var errorMessage = ""
             if content.proxySwitch.isOn && ( socks5Hostname.isEmpty || socks5Port.isEmpty ) {
-                errorMessage = NSLocalizedString("id_socks5_proxy_and_port_must_be", comment: "")
-            } else if content.torSwitch.isOn && !content.proxySwitch.isOn {
-                errorMessage = NSLocalizedString("id_please_set_and_enable_socks5", comment: "")
+                let errorMessage = NSLocalizedString("id_socks5_proxy_and_port_must_be", comment: "")
+                let alert = UIAlertController(title: NSLocalizedString("id_warning", comment: ""), message: errorMessage, preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: NSLocalizedString("id_ok", comment: ""), style: .default) { _ in })
+                present(alert, animated: true, completion: nil)
+                return
             } else {
                 settings = ["network": selectedNetwork,
                             "proxy": content.proxySwitch.isOn,
                             "tor": content.torSwitch.isOn,
                             "socks5_hostname": socks5Hostname,
                             "socks5_port": socks5Port]
-            }
-            if !errorMessage.isEmpty {
-                let alert = UIAlertController(title: NSLocalizedString("id_warning", comment: ""), message: errorMessage, preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: NSLocalizedString("id_ok", comment: ""), style: .default) { _ in })
-                present(alert, animated: true, completion: nil)
-                return
             }
         }
 
