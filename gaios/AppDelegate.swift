c7625a05c6aaa0e46b4877c1cd9e67ed6a7065c7
Luca Vaccaro <luck87@gmail.com>
2020-02-07 14:11:40 +0100

    Disable reconnection attempts on background

--- gaios/AppDelegate.swift
+++ gaios/AppDelegate.swift
@@ -124,7 +124,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        try? AppDelegate.service.getSession().reconnectHint(hint: ["tor_sleep_hint": "sleep"])
+        try? AppDelegate.service.getSession().reconnectHint(hint: ["tor_sleep_hint": "sleep", "hint": "disable"])
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
