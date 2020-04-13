dbf5dbe237bad078a5f9632d9173fbe6225243ae
Luca Vaccaro <luck87@gmail.com>
2020-04-13 11:18:45 +0200

    Update assets for gdk 0.0.30

--- app/prepare_gdk_clang.sh
+++ app/prepare_gdk_clang.sh
@@ -12,7 +12,7 @@ if [ -d gdk ]; then
 else
     git clone https://github.com/Blockstream/gdk.git
     cd gdk
-    git checkout tags/release_0.0.29 -b release_0.0.29
+    git checkout tags/release_0.0.30 -b release_0.0.30
 fi
 
 python3 -m virtualenv -p python3 venv
