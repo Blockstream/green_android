f46b302e64250848431830cfd4066aeee54373a3
Luca Vaccaro <me@lvaccaro.com>
2021-01-16 16:10:11 +0100

    Add iphone simulator in fetch build script

--- tools/fetch_gdk_binaries.sh
+++ tools/fetch_gdk_binaries.sh
@@ -4,9 +4,19 @@ set -e
 
 # The version of gdk to fetch and its sha256 checksum for integrity checking
 NAME="gdk-iphone"
-TARBALL="${NAME}.tar.gz"
-URL="https://github.com/Blockstream/gdk/releases/download/release_0.0.38/${TARBALL}"
 SHA256="87124be9ca82c9000a5e6e82e17e61ded816a9782f167c1079ea8029e279ba15"
+
+if [[ $1 == "--simulator" ]]; then
+    # Get version for iphone simulator
+    NAME="gdk-iphone-sim"
+    SHA256="041a056e86bb6c6f978d920dfa7138592067100acfed9bfbe4926caba3044d81"
+fi
+
+# Setup gdk version and url
+VERSION="release_0.0.38"
+TARBALL="${NAME}.tar.gz"
+URL="https://github.com/Blockstream/gdk/releases/download/${VERSION}/${TARBALL}"
+
 # Pre-requisites
 function check_command() {
     command -v $1 >/dev/null 2>&1 || { echo >&2 "$1 not found, exiting."; exit 1; }
@@ -29,4 +39,7 @@ curl -sL -o ${TARBALL} "${URL}"
 echo "${SHA256}  ${TARBALL}" | shasum -a 256 --check
 tar xf ${TARBALL}
 rm ${TARBALL}
+if [[ $1 == "--simulator" ]]; then
+    mv -f ${NAME} "gdk-iphone"
+fi
 
