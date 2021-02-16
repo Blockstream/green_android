1e242edebdc24af16e72c9b28b2aac0e3b49a255
Luca Vaccaro <me@lvaccaro.com>
2021-02-16 19:34:41 +0100

    jade: move sigToDer & bip32KeyFromParentToBase58 into Wally file

--- gaios/HardwareWallets/Jade/Jade.swift
+++ gaios/HardwareWallets/Jade/Jade.swift
@@ -80,23 +80,10 @@ final class Jade: JadeDevice, HWResolverProtocol {
             .flatMap { res -> Observable<String> in
                 let encoded = res["result"] as? String
                 let decoded = Data(base64Encoded: encoded!)
-                let der = try self.sigToDer(sigDecoded: Array(decoded!))
+                let der = try sigToDer(sigDecoded: Array(decoded!))
                 let hexSig = der.map { String(format: "%02hhx", $0) }.joined()
                 return Observable.just(hexSig)
             }
-    }
-
-    public func sigToDer(sigDecoded: [UInt8]) throws -> [UInt8] {
-        let sig = sigDecoded[1..<sigDecoded.count]
-        let sigPtr: UnsafePointer<UInt8> = UnsafePointer(Array(sig))
-        let derPtr = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(EC_SIGNATURE_DER_MAX_LEN))
-        var written: Int = 0
-        if wally_ec_sig_to_der(sigPtr, sig.count, derPtr, Int(EC_SIGNATURE_DER_MAX_LEN), &written) != WALLY_OK {
-            throw GaError.GenericError
-        }
-        let der = Array(UnsafeBufferPointer(start: derPtr, count: written))
-        //derPtr.deallocate()
-        return der
     }
 
     func xpubs(paths: [[Int]]) -> Observable<[String]> {
@@ -310,32 +297,6 @@ final class Jade: JadeDevice, HWResolverProtocol {
                 }
                 return false
             }
-    }
-
-    public func bip32KeyFromParentToBase58(isMainnet: Bool = true, pubKey: [UInt8], chainCode: [UInt8], branch: UInt32 ) throws -> String {
-        let version = isMainnet ? BIP32_VER_MAIN_PUBLIC : BIP32_VER_TEST_PUBLIC
-        var subactkey: UnsafeMutablePointer<ext_key>?
-        var branchkey: UnsafeMutablePointer<ext_key>?
-        var xpubPtr: UnsafeMutablePointer<Int8>?
-        let pubKey_: UnsafePointer<UInt8> = UnsafePointer(pubKey)
-        let chainCode_: UnsafePointer<UInt8> = UnsafePointer(chainCode)
-        defer {
-            bip32_key_free(subactkey)
-            bip32_key_free(branchkey)
-            wally_free_string(xpubPtr)
-        }
-
-        if bip32_key_init(UInt32(version), UInt32(0), UInt32(0), chainCode_, chainCode.count,
-                                 pubKey_, pubKey.count, nil, 0, nil, 0, nil, 0, subactkey) != WALLY_OK {
-            throw GaError.GenericError
-        }
-        if bip32_key_from_parent(subactkey, branch, UInt32(BIP32_FLAG_KEY_PUBLIC | BIP32_FLAG_SKIP_HASH), branchkey) != WALLY_OK {
-            throw GaError.GenericError
-        }
-        if bip32_key_to_base58(branchkey, UInt32(BIP32_FLAG_KEY_PUBLIC), &xpubPtr) != WALLY_OK {
-            throw GaError.GenericError
-        }
-        return String(cString: xpubPtr!)
     }
 
     // Get (receive) green address
--- gaios/Helpers/Wally.swift
+++ gaios/Helpers/Wally.swift
@@ -13,3 +13,42 @@ public func hexToData(_ hex: String) -> Data {
         buffer.deallocate()
     }))
 }
+
+public func sigToDer(sigDecoded: [UInt8]) throws -> [UInt8] {
+    let sig = sigDecoded[1..<sigDecoded.count]
+    let sigPtr: UnsafePointer<UInt8> = UnsafePointer(Array(sig))
+    let derPtr = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(EC_SIGNATURE_DER_MAX_LEN))
+    var written: Int = 0
+    if wally_ec_sig_to_der(sigPtr, sig.count, derPtr, Int(EC_SIGNATURE_DER_MAX_LEN), &written) != WALLY_OK {
+        throw GaError.GenericError
+    }
+    let der = Array(UnsafeBufferPointer(start: derPtr, count: written))
+    //derPtr.deallocate()
+    return der
+}
+
+public func bip32KeyFromParentToBase58(isMainnet: Bool = true, pubKey: [UInt8], chainCode: [UInt8], branch: UInt32 ) throws -> String {
+    let version = isMainnet ? BIP32_VER_MAIN_PUBLIC : BIP32_VER_TEST_PUBLIC
+    var subactkey: UnsafeMutablePointer<ext_key>?
+    var branchkey: UnsafeMutablePointer<ext_key>?
+    var xpubPtr: UnsafeMutablePointer<Int8>?
+    let pubKey_: UnsafePointer<UInt8> = UnsafePointer(pubKey)
+    let chainCode_: UnsafePointer<UInt8> = UnsafePointer(chainCode)
+    defer {
+        bip32_key_free(subactkey)
+        bip32_key_free(branchkey)
+        wally_free_string(xpubPtr)
+    }
+
+    if bip32_key_init(UInt32(version), UInt32(0), UInt32(0), chainCode_, chainCode.count,
+                             pubKey_, pubKey.count, nil, 0, nil, 0, nil, 0, subactkey) != WALLY_OK {
+        throw GaError.GenericError
+    }
+    if bip32_key_from_parent(subactkey, branch, UInt32(BIP32_FLAG_KEY_PUBLIC | BIP32_FLAG_SKIP_HASH), branchkey) != WALLY_OK {
+        throw GaError.GenericError
+    }
+    if bip32_key_to_base58(branchkey, UInt32(BIP32_FLAG_KEY_PUBLIC), &xpubPtr) != WALLY_OK {
+        throw GaError.GenericError
+    }
+    return String(cString: xpubPtr!)
+}
