d64153622063986fe8ed4f6c142d21855fcddbe5
Angelos Veglektsis <angelos@aveworks.com>
2021-02-23 17:45:26 +0200

    Move bump_gdk.sh to :crypto  module Fix for macOS

--- crypto/bump_gdk.sh
+++ crypto/bump_gdk.sh
@@ -16,8 +16,8 @@ check_command shasum
 
 TAGNAME=$(curl https://api.github.com/repos/blockstream/gdk/releases/latest | jq -r .tag_name)
 NAME=$(curl https://api.github.com/repos/blockstream/gdk/releases/latest | jq -r .name)
-sed -i "s/TAGNAME=.*/TAGNAME=\"${TAGNAME}\"/" ./app/fetch_gdk_binaries.sh
-sed -i "s/TAGNAME=.*/TAGNAME=\"${TAGNAME}\"/" ./app/prepare_gdk_clang.sh
+sed -i '' -e "s/TAGNAME=.*/TAGNAME=\"${TAGNAME}\"/" ./crypto/fetch_gdk_binaries.sh
+sed -i '' -e "s/TAGNAME=.*/TAGNAME=\"${TAGNAME}\"/" ./crypto/prepare_gdk_clang.sh
 
 TEMP=$(mktemp)
 TARURL=$(curl https://api.github.com/repos/blockstream/gdk/releases/latest | jq -r .assets[].browser_download_url | grep gdk-android-jni)
@@ -25,8 +25,8 @@ TARURL=$(curl https://api.github.com/repos/blockstream/gdk/releases/latest | jq 
 curl -sL -o $TEMP $TARURL
 SHA256=$(shasum -a256 $TEMP | awk '{print $1;}')
 echo $SHA256
-sed -i "s/SHA256=.*/SHA256=\"${SHA256}\"/" ./app/fetch_gdk_binaries.sh
+sed -i '' -e "s/SHA256=.*/SHA256=\"${SHA256}\"/" ./crypto/fetch_gdk_binaries.sh
 
 
-git add ./app/fetch_gdk_binaries.sh ./app/prepare_gdk_clang.sh
+git add ./crypto/fetch_gdk_binaries.sh ./crypto/prepare_gdk_clang.sh
 git commit -m "update gdk to ${NAME}" -S
