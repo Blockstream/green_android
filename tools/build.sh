0e402efc6ca68aef068d2f47f724b1cf8545449b
Luca Vaccaro <me@lvaccaro.com>
2021-05-18 22:41:45 +0200

    ci: hide gem bin path inside current PATH

--- tools/build.sh
+++ tools/build.sh
@@ -23,9 +23,6 @@ while true; do
     esac
 done
 
-export GEM_HOME=$HOME/.gem/ruby/2.3.0
-export PATH=$GEM_HOME/bin:$PATH
-
 if [[ "$UPDATE_COCOAPODS" -eq 1 ]]; then
     gem install cocoapods --user-install
     $(which pod) repo update
