ae519bf7b9079cda25f6d818cac2654abed43ec2
Jamie C. Driver <jamie@blockstream.io>
2021-06-15 10:58:37 +0100

    Extend Jade version string parser for more semver-like strings
    Prefer a dash separator between version numbers and pre-release label,
    as this is more semver-compatible and is preferred by some apps.

--- hardware/src/main/java/com/greenaddress/jade/entities/JadeVersion.java
+++ hardware/src/main/java/com/greenaddress/jade/entities/JadeVersion.java
@@ -5,23 +5,24 @@ import androidx.annotation.NonNull;
 import java.util.Objects;
 
 // A Jade version is three dotted (unsigned) integers, followed by an optional
-// string label.  Number parts are numeric compared, label is lex-compared.
-// NOTE: no-string-label is 'greater than' any label.  eg:
-// 1.2.3 > 1.2.3.beta2 > 1.2.3.beta1 > 1.2.3.alpha2 > 1.2.3.alpha1 > 1.2.2[.<anything>]
+// string pre-release label.  Number parts are numeric compared, pre-release label is lex-compared.
+// The pre-release label may be separated by a dot (legacy/deprecated) or a dash (new, more semver-like).
+// NOTE: no prerelease label is 'greater than' any prerelease label.  eg:
+// 1.2.3 > 1.2.3-beta2 > 1.2.3-beta1 > 1.2.3-alpha2 > 1.2.3-alpha1 > 1.2.2[.<anything>]
 //
 // NOTE: we also need to support the fw testing case where there may be a number of commits and
-// a commit-id - eg: 0.21.1-3-gabcdef, 0.21.1-3-gabcdef-dirty, or even 0.21.1.alpha1-3-gabcdef-dirty !
-// In this case we collect the number of commits but discard anything following that.
+// a commit-id - eg: 0.21.1-dirty, 0.21.1-3-gabcdef, 0.21.1-3-gabcdef-dirty, 0.21.1-alpha1-dirty, 0.21.1-alpha1-3-gabcdef-dirty ...
+// In this case we collect any trailing information as a final string to compare on a best-efforts basis.
 //
 public class JadeVersion implements Comparable<JadeVersion> {
     final int major;
     final int minor;
     final int patch;
-    final String label;
-    final int extra_commits;
+    final String pre_release;
+    final String trailer;
 
     public JadeVersion(final String version) {
-        final String[] parts = version.split("-", 3);
+        final String[] parts = version.split("-", 2);
 
         final String[] tag = parts[0].split("[\\.]", 4);
         if (tag.length < 3) {
@@ -33,20 +34,38 @@ public class JadeVersion implements Comparable<JadeVersion> {
         this.minor = Integer.parseInt(tag[1]);
         this.patch = Integer.parseInt(tag[2]);
 
-        // Get any trailing label (alpha1, beta2, rc1, etc.)
-        this.label = tag.length == 4 ? tag[3] : "";
-
-        // See if there were any trailing commits - if so grab those also (and discard commit-id etc)
-        this.extra_commits = parts.length > 1 ? Integer.parseInt(parts[1]) : 0;
+        // Get any trailing pre-release label (alpha1, beta2, rc1, etc.)
+        if (tag.length == 4) {
+            // Legacy/deprecated format with pre-release label after a dot - eg. 1.2.3.beta1[-2-abcdef[-dirty]])
+            this.pre_release = tag[3];
+            this.trailer = parts.length > 1 ? parts[1] : "";
+        }
+        else if (parts.length > 1) {
+            // New format (more semver-like) with '-' separator after a three-part dotted label - eg. 1.2.3[-beta1][-2-abcdef][-dirty]
+            if (parts[1].matches("([0-9]+-.+)|(dirty$)")) {
+                // eg. 1.2.3-2-gabcdef[-dirty] or 1.2.3-dirty
+                this.pre_release = "";
+                this.trailer = parts[1];
+            } else {
+                // eg. 1.2.3-beta1[-2-abcdef][-dirty]
+                final String[] parts2 = parts[1].split("-", 2);
+                this.pre_release = parts2[0];
+                this.trailer = parts2.length > 1 ? parts2[1] : "";
+            }
+        } else {
+            // No trailing info after the three-part dotted label - eg. 1.2.3
+            this.pre_release = "";
+            this.trailer = "";
+        }
     }
 
     @NonNull
     @Override
     public String toString() {
-        final String ver = major + "." + minor + "." + patch;
-        final String trailing = label.length() > 0 ? "." + label : "";
-        final String commits = extra_commits > 0 ? "-" + extra_commits : "";
-        return ver + trailing + commits;
+        final String ver = this.major + "." + this.minor + "." + this.patch;
+        final String prerelease_label = this.pre_release.length() > 0 ? "-" + this.pre_release : "";
+        final String dev_trailer = this.trailer.length() > 0 ? "-" + this.trailer : "";
+        return ver + prerelease_label + dev_trailer;
     }
 
     @Override
@@ -54,16 +73,16 @@ public class JadeVersion implements Comparable<JadeVersion> {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         final JadeVersion that = (JadeVersion) o;
-        return major == that.major &&
-                minor == that.minor &&
-                patch == that.patch &&
-                extra_commits == that.extra_commits &&
-                label.equals(that.label);
+        return this.major == that.major &&
+                this.minor == that.minor &&
+                this.patch == that.patch &&
+                this.pre_release.equals(that.pre_release) &&
+                this.trailer.equals(that.trailer);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(major, minor, patch, extra_commits, label);
+        return Objects.hash(this.major, this.minor, this.patch, this.pre_release, this.trailer);
     }
 
     @Override
@@ -82,25 +101,34 @@ public class JadeVersion implements Comparable<JadeVersion> {
 
         // Versions same - check label
 
-        // Note: any-label is 'less than' no-label
-        // (as a label represents alpha2, beta3, rc1, etc)
-        // eg. 1.2.3.beta3 < 1.2.3 and 1.2.3.rc1 < 1.2.3 etc.
-        if (this.label.length() > 0 && other.label.length() == 0) {
+        // Note: any pre-release label is 'less than' no pre_release label
+        // (as a pre-release label represents alpha2, beta3, rc1, etc)
+        // eg. 1.2.3-beta3 < 1.2.3 and 1.2.3-rc1 < 1.2.3 etc.
+        if (this.pre_release.length() > 0 && other.pre_release.length() == 0) {
             return -1;
         }
-        if (this.label.length() == 0 && other.label.length() > 0) {
+        if (this.pre_release.length() == 0 && other.pre_release.length() > 0) {
             return 1;
         }
 
-        // Both have a label (or both have no label) - lexi-compare label
-        // eg. 1.2.3.alpha3 < 1.2.3.beta2 < 1.2.3.rc1 < 1.2.3.rc2 etc.
-        final int lblcmp = this.label.compareTo(other.label);
+        // Both have a pre-release label (or neither do) - lexi-compare pre-release label
+        // eg. 1.2.3-alpha3 < 1.2.3-beta2 < 1.2.3-rc1 < 1.2.3-rc2 etc.
+        final int lblcmp = this.pre_release.compareTo(other.pre_release);
         if (lblcmp != 0) {
             return lblcmp;
         }
 
-        // Finally compare the number of additional commits
+        // Finally compare the 'dev trailer' - this isn't very meaningful, but is a best-efforts
+        // to provide some sort of deterministic ordering to dev builds.
+        // It means that:
+        // 'X-dirty' > 'X'
+        // 'X-2-gabcdef' > X-1-gabcdef' > 'X'
+        //
+        // Questionable:
+        // It ranks 'X-dirty' as > 'X-3-gabcdef'
+        // It's arbitrary how 'X-2-<commit-id-1>' ranks against 'X-2-<commit-id-2>'
+        // But these are pretty vague and meaningless comparisons anyway.
         // NOTE: we should only get to this when testing local dev jade fw builds.
-        return this.extra_commits - other.extra_commits;
+        return this.trailer.compareTo(other.trailer);
     }
 }
--- hardware/src/test/java/com/greenaddress/jade/entities/JadeVersionTest.java
+++ hardware/src/test/java/com/greenaddress/jade/entities/JadeVersionTest.java
@@ -12,36 +12,92 @@ public class JadeVersionTest {
     @Test
     public void test_ctor_parse() {
         // Normal version number
-        final JadeVersion ver1 = new JadeVersion("0.1.21");
-        assertEquals(0, ver1.major);
-        assertEquals(1, ver1.minor);
-        assertEquals(21, ver1.patch);
-        assertEquals("", ver1.label);
-        assertEquals(0, ver1.extra_commits);
+        final JadeVersion ver0 = new JadeVersion("0.1.21");
+        assertEquals(0, ver0.major);
+        assertEquals(1, ver0.minor);
+        assertEquals(21, ver0.patch);
+        assertEquals("", ver0.pre_release);
+        assertEquals("", ver0.trailer);
 
-        // Version number with label
-        final JadeVersion ver2 = new JadeVersion("0.1.22.beta4");
+        // Version number, dirty
+        final JadeVersion ver1 = new JadeVersion("3.5.4-dirty");
+        assertEquals(3, ver1.major);
+        assertEquals(5, ver1.minor);
+        assertEquals(4, ver1.patch);
+        assertEquals("", ver1.pre_release);
+        assertEquals("dirty", ver1.trailer);
+
+        // Version number with pre-release label (new/semver/dash)
+        final JadeVersion ver2 = new JadeVersion("0.1.22-beta4");
         assertEquals(0, ver2.major);
         assertEquals(1, ver2.minor);
         assertEquals(22, ver2.patch);
-        assertEquals("beta4", ver2.label);
-        assertEquals(0, ver2.extra_commits);
+        assertEquals("beta4", ver2.pre_release);
+        assertEquals("", ver2.trailer);
+
+        // Version number with pre-release label  (legacy/deprecated/dotted)
+        final JadeVersion ver2a = new JadeVersion("0.2.11.rc2");
+        assertEquals(0, ver2a.major);
+        assertEquals(2, ver2a.minor);
+        assertEquals(11, ver2a.patch);
+        assertEquals("rc2", ver2a.pre_release);
+        assertEquals("", ver2a.trailer);
+
+        // Version number with pre-release label new/semver/dash)
+        final JadeVersion ver3 = new JadeVersion("0.1.22-beta4-dirty");
+        assertEquals(0, ver3.major);
+        assertEquals(1, ver3.minor);
+        assertEquals(22, ver3.patch);
+        assertEquals("beta4", ver3.pre_release);
+        assertEquals("dirty", ver3.trailer);
+
+        // Version number with pre-release label (legacy/deprecated/dotted)
+        final JadeVersion ver3a = new JadeVersion("0.2.11.rc2-dirty");
+        assertEquals(0, ver3a.major);
+        assertEquals(2, ver3a.minor);
+        assertEquals(11, ver3a.patch);
+        assertEquals("rc2", ver3a.pre_release);
+        assertEquals("dirty", ver3a.trailer);
 
         // Version number with additional commits
-        final JadeVersion ver3 = new JadeVersion("1.3.2-7-gabcdef");
-        assertEquals(1, ver3.major);
-        assertEquals(3, ver3.minor);
-        assertEquals(2, ver3.patch);
-        assertEquals("", ver3.label);
-        assertEquals(7, ver3.extra_commits);
-
-        // Version number with label and additional commits
-        final JadeVersion ver4 = new JadeVersion("1.5.23.rc1-4-gabcdef-dirty");
+        final JadeVersion ver4 = new JadeVersion("1.3.2-7-gabcdef");
         assertEquals(1, ver4.major);
-        assertEquals(5, ver4.minor);
-        assertEquals(23, ver4.patch);
-        assertEquals("rc1", ver4.label);
-        assertEquals(4, ver4.extra_commits);
+        assertEquals(3, ver4.minor);
+        assertEquals(2, ver4.patch);
+        assertEquals("", ver4.pre_release);
+        assertEquals("7-gabcdef", ver4.trailer);
+
+        // Version number with pre-release label and additional commits (new/semver/dash)
+        final JadeVersion ver5 = new JadeVersion("1.5.23.rc1-4-gabcdef");
+        assertEquals(1, ver5.major);
+        assertEquals(5, ver5.minor);
+        assertEquals(23, ver5.patch);
+        assertEquals("rc1", ver5.pre_release);
+        assertEquals("4-gabcdef", ver5.trailer);
+
+        // Version number with pre-release label and additional commits (legacy/deprecated/dotted)
+        final JadeVersion ver5a = new JadeVersion("3.7.16.rc2-3-gabcdef");
+        assertEquals(3, ver5a.major);
+        assertEquals(7, ver5a.minor);
+        assertEquals(16, ver5a.patch);
+        assertEquals("rc2", ver5a.pre_release);
+        assertEquals("3-gabcdef", ver5a.trailer);
+
+        // Version number with pre-release label and additional commits (new/semver/dash)
+        final JadeVersion ver6 = new JadeVersion("1.5.23.rc1-4-gabcdef-dirty");
+        assertEquals(1, ver6.major);
+        assertEquals(5, ver6.minor);
+        assertEquals(23, ver6.patch);
+        assertEquals("rc1", ver6.pre_release);
+        assertEquals("4-gabcdef-dirty", ver6.trailer);
+
+        // Version number with pre-release label and additional commits (legacy/deprecated/dotted)
+        final JadeVersion ver6a = new JadeVersion("3.7.16.rc2-3-gabcdef-dirty");
+        assertEquals(3, ver6a.major);
+        assertEquals(7, ver6a.minor);
+        assertEquals(16, ver6a.patch);
+        assertEquals("rc2", ver6a.pre_release);
+        assertEquals("3-gabcdef-dirty", ver6a.trailer);
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -64,18 +120,17 @@ public class JadeVersionTest {
         new JadeVersion("1.2.x");
     }
 
-    @Test(expected = NumberFormatException.class)
-    public void test_bad_commits_parse() {
-        new JadeVersion("1.2.3-x-gabcdef");
-    }
-
     @Test
     public void check_string_representation() {
         final String[] versions = new String[]{
                 "0.1.21",
+                "0.1.21-dirty",
                 "1.3.8-13-gabcdef",
-                "2.7.11.alpha2",
-                "3.17.34.beta12-43-gabcdef",
+                "1.3.8-13-gabcdef-dirty",
+                "2.7.11-alpha2",
+                "2.7.11-alpha2-dirty",
+                "4.1.3-beta3-43-gabcdef",
+                "3.17.34-rc2-143-gabcdef-dirty",
         };
 
         for (final String version : versions) {
@@ -83,8 +138,39 @@ public class JadeVersionTest {
             final JadeVersion ver = new JadeVersion(version);
             final String str = ver.toString();
 
-            // Generated string should be prefix part of original string
-            assertEquals(version.substring(0,str.length()), str);
+            // Generated string should be same as original string
+            assertEquals(version, str);
+
+            // Should be able to re-generate same version from string representation
+            final JadeVersion same = new JadeVersion(str);
+            assertEquals(0, ver.compareTo(same));
+            assertEquals(0, same.compareTo(ver));
+            assertEquals(str, same.toString());
+            assertEquals(ver, same);
+        }
+    }
+
+    @Test
+    public void check_legacy_string_representation() {
+        // This format is what we used originally, but is now deprecated in favour
+        // of the dash-separator, as this is more semver-like.
+        // NOTE: all these have the 'offending' dot at ver[7] for simplicity
+        final String[] versions = new String[]{
+                "2.17.23.alpha2",
+                "2.17.23.beta1-dirty",
+                "11.1.34.beta12-43-gabcdef",
+                "7.10.18.rc1-8-gabcdef-dirty"
+        };
+
+        for (final String version : versions) {
+            // Check converting to/from string gets the same version back
+            final JadeVersion ver = new JadeVersion(version);
+            final String str = ver.toString();
+
+            // Generated string should be same as original string, *BUT* the dot before
+            // the pre-release label would have been changed to a dash (at pos 7)
+            final String expected = version.substring(0, 7) + '-' + version.substring(8);
+            assertEquals(expected, str);
 
             // Should be able to re-generate same version from string representation
             final JadeVersion same = new JadeVersion(str);
@@ -112,7 +198,7 @@ public class JadeVersionTest {
 
             // For all later versions, check they are 'greater than' this
             for (int j = i+1; j < versions.length; ++j) {
-                assertTrue(versions[j].compareTo(versions[i]) > 0);
+                assertTrue(""+i+"/"+j, versions[j].compareTo(versions[i]) > 0);
                 assertTrue(versions[i].compareTo(versions[j]) < 0);
                 assertNotEquals(versions[i], versions[j]);
             }
@@ -124,11 +210,13 @@ public class JadeVersionTest {
         final JadeVersion[] versions = new JadeVersion[]{
                 new JadeVersion("0.1.5"),
                 new JadeVersion("0.1.9"),
+                new JadeVersion("0.1.9-dirty"),
                 new JadeVersion("0.1.10"),
                 new JadeVersion("0.1.11"),
                 new JadeVersion("0.9.1"),
                 new JadeVersion("0.10.0"),
                 new JadeVersion("1.0.0"),
+                new JadeVersion("1.0.0-dirty"),
                 new JadeVersion("1.0.1"),
                 new JadeVersion("1.1.0")
         };
@@ -136,14 +224,20 @@ public class JadeVersionTest {
     }
 
     @Test
-    public void test_labels_compare() {
+    public void test_pre_release_labels_compare() {
         final JadeVersion[] versions = new JadeVersion[]{
                 new JadeVersion("0.1.20"),
-                new JadeVersion("0.1.21.alpha1"),
+                new JadeVersion("0.1.20-dirty"),
+                new JadeVersion("0.1.21-alpha1"),
+                new JadeVersion("0.1.21-alpha1-dirty"),
                 new JadeVersion("0.1.21.alpha2"),
                 new JadeVersion("0.1.21.beta1"),
-                new JadeVersion("0.1.21.beta2"),
+                new JadeVersion("0.1.21-beta2"),
+                new JadeVersion("0.1.21.beta2-dirty"),
                 new JadeVersion("0.1.21.rc1"),
+                new JadeVersion("0.1.21-rc2"),
+                new JadeVersion("0.1.21.rc3"),
+                new JadeVersion("0.1.21-rc4"),
                 new JadeVersion("0.1.21")
         };
         check_versions_array(versions);
@@ -155,9 +249,42 @@ public class JadeVersionTest {
                 new JadeVersion("0.1.21"),
                 new JadeVersion("0.1.21-1-gabcdef-dirty"),
                 new JadeVersion("0.1.21-3-gabcdef"),
-                new JadeVersion("0.1.21-7-gabcdef-dirty"),
+                new JadeVersion("0.1.21-3-gabcdef-dirty"),
+                new JadeVersion("0.1.21-7-gabcdef"),
                 new JadeVersion("0.1.22"),
-                new JadeVersion("0.1.22-1-gabcdef")
+                new JadeVersion("0.1.22-1-gabcdef"),
+                new JadeVersion("0.1.22-dirty")
+        };
+        check_versions_array(versions);
+    }
+
+    @Test
+    public void test_questionable_dev_compare() {
+        // These cases are of questionable value and may not be ideal - this
+        // test case is just here to demonstrate/confirm these comparisons.
+        final JadeVersion[] versions = new JadeVersion[]{
+                new JadeVersion("0.2.12"),
+
+                // Same number number of commits - comparison works on commit-id string/hex,
+                // which is pretty meaningless, but at least consistent/deterministic.
+                new JadeVersion("0.2.12-1-gabcdef"),
+                new JadeVersion("0.2.12-1-gdefabc"),
+                new JadeVersion("0.2.12-2-gabcdef"),
+                new JadeVersion("0.2.12-2-gdefabc"),
+                new JadeVersion("0.2.13-beta1-1-gabcdef"),
+                new JadeVersion("0.2.13-beta1-1-gdefabc"),
+                new JadeVersion("0.2.13-rc1-1-gabcdef-dirty"),
+                new JadeVersion("0.2.13-rc1-1-gdefabc"),
+
+                // '-dirty' is seen as 'greater-than' '-1-gabcdef' which is perhaps suboptimal,
+                // but really also pretty meaningless.  Again we just settle for consistent ordering.
+                new JadeVersion("0.2.13"),
+                new JadeVersion("0.2.13-3-gbcdefa"),
+                new JadeVersion("0.2.13-dirty"),
+                new JadeVersion("0.2.14-rc1-3-gbcdefa"),
+                new JadeVersion("0.2.14-rc1-dirty"),
+                new JadeVersion("0.2.14"),
+                new JadeVersion("0.2.14-dirty")
         };
         check_versions_array(versions);
     }
@@ -168,15 +295,23 @@ public class JadeVersionTest {
                 new JadeVersion("0.1.21"),
                 new JadeVersion("0.1.21-1-gabcdef"),
                 new JadeVersion("0.1.21-13-gabcdef"),
-                new JadeVersion("0.1.22.alpha1"),
-                new JadeVersion("0.1.22.alpha1-1-gabcdef-dirty"),
+                new JadeVersion("0.1.21-dirty"),
+                new JadeVersion("0.1.22-alpha1"),
+                new JadeVersion("0.1.22-alpha1-1-gabcdef"),
+                new JadeVersion("0.1.22-alpha1-1-gabcdef-dirty"),
+                new JadeVersion("0.1.22-alpha1-dirty"),
                 new JadeVersion("0.1.22.alpha2"),
-                new JadeVersion("0.1.22.beta1"),
-                new JadeVersion("0.1.22.beta1-1-gabcdef-dirty"),
+                new JadeVersion("0.1.22-beta1"),
+                new JadeVersion("0.1.22-beta1-1-gabcdef"),
+                new JadeVersion("0.1.22-beta1-1-gdefabc"),
+                new JadeVersion("0.1.22-beta1-1-gdefabc-dirty"),
+                new JadeVersion("0.1.22-beta1-dirty"),
                 new JadeVersion("0.1.22.rc1"),
-                new JadeVersion("0.1.22.rc1-1-gabcdef"),
+                new JadeVersion("0.1.22-rc1-1-gabcdef"),
+                new JadeVersion("0.1.22.rc1-dirty"),
                 new JadeVersion("0.1.22")
         };
         check_versions_array(versions);
     }
+
 }
