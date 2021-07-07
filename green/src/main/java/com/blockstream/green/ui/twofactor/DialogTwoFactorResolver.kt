b8b404ee6d440f83864431a626348345b8413274
Angelos Veglektsis <angelos@aveworks.com>
2021-07-07 14:56:24 +0300

    Beautify 2fa code dialog

--- green/src/main/java/com/blockstream/green/ui/twofactor/DialogTwoFactorResolver.kt
+++ green/src/main/java/com/blockstream/green/ui/twofactor/DialogTwoFactorResolver.kt
@@ -6,28 +6,32 @@ import android.content.DialogInterface
 import android.text.InputType
 import android.view.LayoutInflater
 import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.core.widget.doAfterTextChanged
 import com.blockstream.gdk.TwoFactorResolver
 import com.blockstream.green.R
 import com.blockstream.green.databinding.EditTextDialogBinding
+import com.blockstream.green.databinding.TwofactorCodeDialogBinding
+import com.blockstream.green.databinding.TwofactorCodeDialogBindingImpl
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.core.Single
 
-class DialogTwoFactorResolver(val context: Context, val method: String? = null) : TwoFactorResolver {
+class DialogTwoFactorResolver(val context: Context, val method: String? = null) :
+    TwoFactorResolver {
 
     override fun selectMethod(availableMethods: List<String>): Single<String> =
         Single.create<String> { emitter ->
 
             // Method is already selected in the constructor
-            if(method != null){
+            if (method != null) {
                 emitter.onSuccess(method)
                 return@create
             }
 
             val availableMethodsChoices = availableMethods.map {
-                when(it){
+                when (it) {
                     "email" -> context.getString(R.string.id_email)
                     "sms" -> context.getString(R.string.id_sms)
                     "gauth" -> context.getString(R.string.id_authenticator_app)
@@ -42,8 +46,8 @@ class DialogTwoFactorResolver(val context: Context, val method: String? = null) 
                     availableMethodsChoices.toTypedArray(),
                     0, null
                 )
-                .setPositiveButton(android.R.string.ok){ dialogInterface: DialogInterface, _: Int ->
-                    if(dialogInterface is AlertDialog){
+                .setPositiveButton(android.R.string.ok) { dialogInterface: DialogInterface, _: Int ->
+                    if (dialogInterface is AlertDialog) {
                         emitter.onSuccess(availableMethods[dialogInterface.listView.checkedItemPosition])
                     }
                 }
@@ -61,20 +65,19 @@ class DialogTwoFactorResolver(val context: Context, val method: String? = null) 
     override fun getCode(method: String, attemptsRemaining: Int?): Single<String> =
         Single.create<String> { emitter ->
 
-            val dialogBinding = EditTextDialogBinding.inflate(LayoutInflater.from(context))
+            val dialogBinding = TwofactorCodeDialogBinding.inflate(LayoutInflater.from(context))
 
+            dialogBinding.icon.setImageResource(getIconForMethod(method))
+            dialogBinding.title = context.getString(R.string.id_please_provide_your_1s_code, method)
             dialogBinding.hint = context.getString(R.string.id_code)
             dialogBinding.editText.inputType = InputType.TYPE_CLASS_NUMBER
-            attemptsRemaining?.let{
-                dialogBinding.textInputLayout.helperText = context.getString(R.string.id_attempts_remaining_d, it)
+            attemptsRemaining?.let {
+                dialogBinding.textInputLayout.helperText =
+                    context.getString(R.string.id_attempts_remaining_d, it)
             }
 
-           val dialog = MaterialAlertDialogBuilder(context)
-                .setTitle(context.getString(R.string.id_please_provide_your_1s_code, method))
+            val dialog = MaterialAlertDialogBuilder(context)
                 .setView(dialogBinding.root)
-                .setPositiveButton(R.string.id_ok) { _, _ ->
-                    emitter.onSuccess(dialogBinding.text ?: "")
-                }
                 .setNegativeButton(android.R.string.cancel, null)
                 .setCancelable(false)
                 .setOnDismissListener {
@@ -86,7 +89,7 @@ class DialogTwoFactorResolver(val context: Context, val method: String? = null) 
 
             // Auto proceed on 6 digits input
             dialogBinding.editText.doAfterTextChanged {
-                if(it?.length == 6){
+                if (it?.length == 6) {
                     emitter.onSuccess(dialogBinding.text ?: "")
                     dialog.dismiss()
                 }
@@ -97,4 +100,12 @@ class DialogTwoFactorResolver(val context: Context, val method: String? = null) 
 
         }.subscribeOn(AndroidSchedulers.mainThread())
 
+    private fun getIconForMethod(method: String): Int {
+        return when (method) {
+            "email" -> R.drawable.ic_2fa_email
+            "call" -> R.drawable.ic_2fa_call
+            "gauth" -> R.drawable.ic_2fa_authenticator
+            else -> R.drawable.ic_2fa_sms
+        }
+    }
 }
