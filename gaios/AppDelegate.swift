f45ddb0912a21d253d4df5cca5d7016d22e3a719
Luca Vaccaro <me@lvaccaro.com>
2021-09-06 16:07:11 +0200

    gdk: reconnect with hint on foreground & background
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- gaios/AppDelegate.swift
+++ gaios/AppDelegate.swift
@@ -104,17 +104,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        try? SessionManager.shared.reconnectHint(hint: ["tor_sleep_hint": "sleep", "hint": "disable"])
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        DispatchQueue.global().async {
+            try? SessionManager.shared.reconnectHint(hint: ["tor_sleep_hint": "sleep", "hint": "disable"])
+        }
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        DispatchQueue.global(qos: .background).async {
+        DispatchQueue.global().async {
             try? SessionManager.shared.reconnectHint(hint: ["tor_sleep_hint": "wakeup", "hint": "now"])
         }
     }
