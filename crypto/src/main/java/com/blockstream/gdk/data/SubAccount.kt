5f1e5e4d3db5775d603567bbc276ae70f0f34c27
Jamie C. Driver <jamie@blockstream.io>
2021-09-20 10:54:43 +0100

    Add recovery_pub_key to SubAccount data class

--- crypto/src/main/java/com/blockstream/gdk/data/SubAccount.kt
+++ crypto/src/main/java/com/blockstream/gdk/data/SubAccount.kt
@@ -15,6 +15,7 @@ data class SubAccount(
     @SerialName("name") val name: String,
     @SerialName("pointer") val pointer: Long,
     @SerialName("receiving_id") val receivingId: String,
+    @SerialName("recovery_pub_key") val recoveryPubKey: String = "",
     @SerialName("recovery_chain_code") val recoveryChainCode: String = "",
     @Serializable(with = AccountTypeSerializer::class)
     @SerialName("type") val type: AccountType,
--- crypto/src/main/java/com/greenaddress/greenapi/data/SubaccountData.java
+++ crypto/src/main/java/com/greenaddress/greenapi/data/SubaccountData.java
@@ -96,7 +96,7 @@ public class SubaccountData extends JSONData {
     }
 
     public SubAccount toSubAccount(){
-        return new SubAccount(getName(), getPointer(), getReceivingId(), getRecoveryChainCode(), AccountType.Companion.byGDKType(getType()));
+        return new SubAccount(getName(), getPointer(), getReceivingId(), getRecoveryPubKey(), getRecoveryChainCode(), AccountType.Companion.byGDKType(getType()));
     }
 
 }
