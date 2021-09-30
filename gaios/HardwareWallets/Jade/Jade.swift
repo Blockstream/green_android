00b97a2ab55e382c2f253aa046ee644682482ab0
Luca Vaccaro <me@lvaccaro.com>
2021-09-30 03:19:42 +0200

    Explicitly ignore expired certificate in Jade pinserver request
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- gaios/HardwareWallets/Jade/Jade.swift
+++ gaios/HardwareWallets/Jade/Jade.swift
@@ -8,6 +8,7 @@ import SwiftCBOR
 final class Jade: JadeChannel, HWProtocol {
 
     public static let shared = Jade()
+    static let EXCLUDED_CERTIFICATE = "-----BEGIN CERTIFICATE-----\nMIIDSjCCAjKgAwIBAgIQRK+wgNajJ7qJMDmGLvhAazANBgkqhkiG9w0BAQUFADA/\nMSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\nDkRTVCBSb290IENBIFgzMB4XDTAwMDkzMDIxMTIxOVoXDTIxMDkzMDE0MDExNVow\nPzEkMCIGA1UEChMbRGlnaXRhbCBTaWduYXR1cmUgVHJ1c3QgQ28uMRcwFQYDVQQD\nEw5EU1QgUm9vdCBDQSBYMzCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\nAN+v6ZdQCINXtMxiZfaQguzH0yxrMMpb7NnDfcdAwRgUi+DoM3ZJKuM/IUmTrE4O\nrz5Iy2Xu/NMhD2XSKtkyj4zl93ewEnu1lcCJo6m67XMuegwGMoOifooUMM0RoOEq\nOLl5CjH9UL2AZd+3UWODyOKIYepLYYHsUmu5ouJLGiifSKOeDNoJjj4XLh7dIN9b\nxiqKqy69cK3FCxolkHRyxXtqqzTWMIn/5WgTe1QLyNau7Fqckh49ZLOMxt+/yUFw\n7BZy1SbsOFU5Q9D8/RhcQPGX69Wam40dutolucbY38EVAjqr2m7xPi71XAicPNaD\naeQQmxkqtilX4+U9m5/wAl0CAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAOBgNV\nHQ8BAf8EBAMCAQYwHQYDVR0OBBYEFMSnsaR7LHH62+FLkHX/xBVghYkQMA0GCSqG\nSIb3DQEBBQUAA4IBAQCjGiybFwBcqR7uKGY3Or+Dxz9LwwmglSBd49lZRNI+DT69\nikugdB/OEIKcdBodfpga3csTS7MgROSR6cz8faXbauX+5v3gTt23ADq1cEmv8uXr\nAvHRAosZy5Q6XkjEGB5YGV8eAlrwDPGxrancWYaLbumR9YbK+rlmM6pZW87ipxZz\nR8srzJmwN0jP41ZL9c8PDHIyh8bwRLtTcm1D9SZImlJnt1ir/md2cXjbDaJWFBM5\nJDGFoqgCWjBH4d1QB7wCCZAA62RjYJsWvIjJEubSfZGL+T0yjWW06XyxV3bqxbYo\nOb8VZRzI9neWagqNdwvYkQsEjgfbKbYK7p2CNTUQ\n-----END CERTIFICATE-----\n"
     let SIGHASH_ALL: UInt8 = 1
 
     func version() -> Observable<[String: Any]> {
@@ -20,7 +21,12 @@ final class Jade: JadeChannel, HWProtocol {
     }
 
     func httpRequest(_ httpRequest: [String: Any]) -> [String: Any]? {
-        let httpParams = httpRequest["params"] as? [String: Any]
+        // Temporary workaround - prune old cert (from Jade pre- 0.1.28 fw)
+        // until gdk better at ignoring bad/invalid/expired certs
+        var httpParams = httpRequest["params"] as? [String: Any]
+        if let rootCert = httpParams?["root_certificates"] as? [String], rootCert.count == 1 && rootCert.contains(Jade.EXCLUDED_CERTIFICATE) {
+            httpParams?.removeValue(forKey: "root_certificates")
+        }
         let httpResponse = try? SessionManager.shared.httpRequest(params: httpParams ?? [:])
         let httpResponseBody = httpResponse?["body"] as? [String: Any]
         return httpResponseBody
