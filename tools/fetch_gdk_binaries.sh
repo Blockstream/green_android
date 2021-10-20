dd9970208cb44aa92f9248a3e88e77334d501a8f
Luca Vaccaro <me@lvaccaro.com>
2021-10-20 09:23:24 +0200

    gdk: update library to 0.0.46
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- tools/fetch_gdk_binaries.sh
+++ tools/fetch_gdk_binaries.sh
@@ -4,16 +4,16 @@ set -e
 
 # The version of gdk to fetch and its sha256 checksum for integrity checking
 NAME="gdk-iphone"
-SHA256="a9298ba6ace7ad1d5c2402fb2466f6a8712737ce5893aa4c0f1c8c4a53ec1487"
+SHA256="7ccf8bc79445bcac158b9d3f250ec51ce3214e6d933b1aa8aeb79588c8fa51de"
 
 if [[ $1 == "--simulator" ]]; then
     # Get version for iphone simulator
     NAME="gdk-iphone-sim"
-    SHA256="8d237eacaccb62d81166b7f3d54b5ef56764db9c5be3067008bed552565725b9"
+    SHA256="4877da818e739ec8a68e3eacb5cb816f837a59c0f9edcd082d7502cd8efaa378"
 fi
 
 # Setup gdk version and url
-VERSION="release_0.0.45.post1"
+VERSION="release_0.0.46"
 TARBALL="${NAME}.tar.gz"
 URL="https://github.com/Blockstream/gdk/releases/download/${VERSION}/${TARBALL}"
 
