a87fa8b82cd9a6b150ad64c54e01e030a66aca48
Mauro Olivo <olivomauro@gmail.com>
2021-11-23 12:39:25 +0100

    Fix balance convert on liquid assets

--- gaios/Transaction/TableViewCells/TransactionAmountCell.swift
+++ gaios/Transaction/TableViewCells/TransactionAmountCell.swift
@@ -38,29 +38,51 @@ class TransactionAmountCell: UITableViewCell {
             lblRecipient.isHidden = true
         }
         lblRecipient.text = transaction.address()
+        lblAmount.textColor = color
+        lblFiat.textColor = color
 
-        if let balance = Balance.convert(details: ["satoshi": transaction.satoshi]) {
-            let (amount, denom) = balance.get(tag: btc)
-            lblAmount.text = String(format: "%@%@", isOutgoing ? "-" : "+", amount ?? "")
-
-            lblAsset.text = "\(denom)"
-            if let fiat = balance.fiat {
-                lblFiat.text = "≈ \(fiat) \(balance.fiatCurrency)"
-            }
-            lblAmount.textColor = color
-            lblFiat.textColor = color
-
-//            if isBtc || tag == getGdkNetwork("liquid").policyAsset {
-            let (fiat, fiatCurrency) = balance.get(tag: "fiat")
-            lblFiat.text = "≈ \(fiat ?? "N.A.") \(fiatCurrency)"
-//            }
-        }
         if network == "mainnet" {
             icon.image = UIImage(named: "ntw_btc")
         } else if network == "testnet" {
             icon.image = UIImage(named: "ntw_testnet")
         } else {
             icon.image = Registry.shared.image(for: transaction.defaultAsset)
+        }
+
+        if transaction.defaultAsset == btc {
+            if let balance = Balance.convert(details: ["satoshi": transaction.satoshi]) {
+                let (amount, denom) = balance.get(tag: btc)
+                lblAmount.text = String(format: "%@%@", isOutgoing ? "-" : "+", amount ?? "")
+
+                lblAsset.text = "\(denom)"
+                if let fiat = balance.fiat {
+                    lblFiat.text = "≈ \(fiat) \(balance.fiatCurrency)"
+                }
+
+    //            if isBtc || tag == getGdkNetwork("liquid").policyAsset {
+                let (fiat, fiatCurrency) = balance.get(tag: "fiat")
+                lblFiat.text = "≈ \(fiat ?? "N.A.") \(fiatCurrency)"
+    //            }
+            }
+        } else {
+            let amounts = Transaction.sort(transaction.amounts)
+            if let amount = isIncoming ? amounts[0] : amounts.filter({ $0.key == transaction.defaultAsset}).first {
+                let info = Registry.shared.infos[amount.key]
+                let icon = Registry.shared.image(for: amount.key)
+                let tag = amount.key
+                let asset = info ?? AssetInfo(assetId: tag, name: tag, precision: 0, ticker: "")
+                let satoshi = transaction.amounts[amount.key] ?? 0
+                let details = ["satoshi": satoshi, "asset_info": asset.encode()!] as [String: Any]
+
+                if let balance = Balance.convert(details: details) {
+                    let (amount, denom) = balance.get(tag: tag)
+                    lblAmount.text = String(format: "%@%@", isOutgoing ? "-" : "+", amount ?? "")
+                    lblAsset.text = denom
+                    self.icon.image = icon
+                    lblFiat.isHidden = true
+                    lblRecipient.isHidden = true
+                }
+            }
         }
     }
 }
