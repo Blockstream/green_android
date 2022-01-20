d52b45b5974080428b58e9c37dc17ae415d27d25
Mauro Olivo <olivomauro@gmail.com>
2022-01-20 14:45:25 +0100

    fix translation

--- gaios/Extensions/UITextField.swift
+++ gaios/Extensions/UITextField.swift
@@ -38,7 +38,7 @@ extension UITextField {
         doneToolbar.barStyle = UIBarStyle.default
 
         let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
-        let done: UIBarButtonItem = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.done, target: self, action: myAction)
+        let done: UIBarButtonItem = UIBarButtonItem(title: NSLocalizedString("id_done", comment: ""), style: UIBarButtonItem.Style.done, target: self, action: myAction)
 
         var items = [UIBarButtonItem]()
         items.append(flexSpace)
