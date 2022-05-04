36dad751cacf6be2f2dc1d722471de920786740c
Luca Vaccaro <me@lvaccaro.com>
2022-05-04 19:15:33 +0200

    fetch gdk: fetch gdk commit artifacts from gcloud
    Signed-off-by: Luca Vaccaro <me@lvaccaro.com>

--- tools/fetch_gdk_binaries.sh
+++ tools/fetch_gdk_binaries.sh
@@ -2,22 +2,58 @@
 # Downloads and installs the pre-built gdk libraries for use by green_ios
 set -e
 
-# The version of gdk to fetch and its sha256 checksum for integrity checking
-if [[ $1 == "--simulator" ]]; then
-    # Get version for iphone simulator
-    NAME="gdk-iphone-sim"
-    SHA256_IPHONESIM="0ccaecf7c292547d051397bf2b5027ed36efca587721e46ed588b7b05e6d584c"
-    SHA256=${SHA256_IPHONESIM}
-else
-    NAME="gdk-iphone"
-    SHA256_IPHONE="66306a7fe73838b88925066303e65af9b1ca2bada6db90789a153d83bbd62cba"
-    SHA256=${SHA256_IPHONE}
-fi
+# ----- Help
+help_message() {
+  cat <<- _EOF_
+  Downloads and install the pre-built GDK libraries
 
-# Setup gdk version and url
+  Usage: $SCRIPT_NAME [-h|--help] [-c|--commit sha256] [-s|--simulator]
+
+  Options:
+    -c, --commit Download the provided commit
+    -s, --simulator Select iphone simulator platform
+    -h, --help  Display this help message and exit
+
+_EOF_
+  exit 0
+}
+
+# ----- Vars
+NAME="gdk-iphone"
+SHA256="66306a7fe73838b88925066303e65af9b1ca2bada6db90789a153d83bbd62cba"
 TAGNAME="release_0.0.52"
 TARBALL="${NAME}.tar.gz"
 URL="https://github.com/Blockstream/gdk/releases/download/${TAGNAME}/${TARBALL}"
+NAME_IPHONESIM="gdk-iphone-sim"
+SHA256_IPHONESIM="0ccaecf7c292547d051397bf2b5027ed36efca587721e46ed588b7b05e6d584c"
+SIMULATOR=false
+VALIDATE_CHECKSUM=true
+COMMIT=false
+GCLOUD_URL="https://storage.googleapis.com/green-gdk-builds/gdk-"
+
+# --- Argument handling
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -h | --help)
+      help_message ;;
+    -c | --commit)
+      COMMIT=${2}
+      shift 2;;
+    -s | --simulator)
+      SIMULATOR=true
+      shift 1;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]:-}" # restore positional parameters
 
 # Pre-requisites
 function check_command() {
@@ -36,12 +72,30 @@ fi
 # Clean up any previous install
 rm -rf gdk-iphone
 
+if [[ $SIMULATOR == true ]]; then
+    NAME=${NAME_IPHONESIM}
+    SHA256=${SHA256_IPHONESIM}
+    TARBALL="${NAME}.tar.gz"
+    URL="https://github.com/Blockstream/gdk/releases/download/${TAGNAME}/${TARBALL}"
+fi
+
+if [[ $COMMIT != false ]]; then
+  URL="${GCLOUD_URL}${COMMIT}/${TARBALL}"
+  VALIDATE_CHECKSUM=false
+fi
+
 # Fetch, validate and decompress gdk
+echo "Downloading from $URL"
 curl -sL -o ${TARBALL} "${URL}"
-echo "${SHA256}  ${TARBALL}" | shasum -a 256 --check
-tar xf ${TARBALL}
+if [[ $VALIDATE_CHECKSUM = true ]]; then
+  echo "Validating checksum $SHA256"
+  echo "${SHA256}  ${TARBALL}" | shasum -a 256 --check
+fi
+
+tar xvf ${TARBALL}
 rm ${TARBALL}
-if [[ $1 == "--simulator" ]]; then
+
+if [[ $SIMULATOR == true ]]; then
     mv -f ${NAME} "gdk-iphone"
 fi
 
