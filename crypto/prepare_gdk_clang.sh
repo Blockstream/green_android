c1617057ec1981bbc5627500a0042c6faa1a7b5f
Angelos Veglektsis <angelos@aveworks.com>
2022-07-25 14:18:09 +0300

    Remove --lto from GDK build script

--- crypto/prepare_gdk_clang.sh
+++ crypto/prepare_gdk_clang.sh
@@ -34,8 +34,8 @@ mkdir -p ../src/main/java/com/blockstream/libwally ../src/main/java/com/blockstr
 
 for arch in $all_archs; do
     mkdir -p ../src/main/jniLibs/$arch $PWD/gdk-android-jni$arch
-    #./tools/build.sh --buildtype=debug --lto=false --install $PWD/gdk-android-jni$arch --ndk $arch
-    ./tools/build.sh --install $PWD/gdk-android-jni$arch --ndk $arch --lto=false
+    #./tools/build.sh --buildtype=debug --install $PWD/gdk-android-jni$arch --ndk $arch
+    ./tools/build.sh --install $PWD/gdk-android-jni$arch --ndk $arch
     cp gdk-android-jni$arch/lib/$arch/* ../src/main/jniLibs/$arch
     cp gdk-android-jni$arch/java/com/blockstream/libgreenaddress/GDK.java ../src/main/java/com/blockstream/libgreenaddress/GDK.java
     cp gdk-android-jni$arch/java/com/blockstream/libwally/Wally.java ../src/main/java/com/blockstream/libwally/Wally.java
