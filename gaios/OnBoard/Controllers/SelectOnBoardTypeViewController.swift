f0ed6571a1c562567870485191ccffdb2207e369
Mauro Olivo <olivomauro@gmail.com>
2023-03-30 09:58:19 +0200

    Select onboard type: fix background

--- gaios/OnBoard/Controllers/SelectOnBoardTypeViewController.swift
+++ gaios/OnBoard/Controllers/SelectOnBoardTypeViewController.swift
@@ -19,9 +19,21 @@ class SelectOnBoardTypeViewController: UIViewController {
 
     var actionToButton: ActionToButton?
     var iAgree: Bool = false
+    let mash = UIImageView(image: UIImage(named: "il_mash")!)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        mash.alpha = 0.6
+        view.insertSubview(mash, at: 0)
+        mash.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            mash.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            mash.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            mash.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0),
+            mash.heightAnchor.constraint(equalToConstant: view.frame.height / 1.8)
+        ])
 
         iAgree = AccountsRepository.shared.swAccounts.count + AccountsRepository.shared.hwAccounts.count > 0
 
