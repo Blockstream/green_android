a80cd6d0ad74e4883d871a8fd73562c5699dc588
Mauro Olivo <olivomauro@gmail.com>
2023-04-12 08:54:20 +0200

    Refactor mnemonic screen

--- gaios/UserSettings/Cells/WordCell.swift
+++ gaios/UserSettings/Cells/WordCell.swift
@@ -4,9 +4,13 @@ class WordCell: UICollectionViewCell {
 
     @IBOutlet weak var lblNum: UILabel!
     @IBOutlet weak var lblWord: UILabel!
+    @IBOutlet weak var bg: UIView!
 
     func configure(num: Int, word: String) {
         self.lblWord.text = word
         self.lblNum.text = "\(num + 1)"
+        bg.borderWidth = 1.0
+        bg.borderColor = .white.withAlphaComponent(0.2)
+        bg.layer.cornerRadius = 3.0
     }
 }
