e38570c7621e7e2a2b3d6a3172669ffdb960650a
Angelos Veglektsis <angelos@aveworks.com>
2024-01-08 19:22:11 +0200

    Increment to version 4.0.23

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,15 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [4.0.23] - 2024-01-08
+
+### Changed
+- Updated project dependencies
+- Improve build reproducibility
+
+### Fixed
+- Various bug and crash fixes
+
 ## [4.0.22] - 2023-12-19
 
 ### Changed
--- green/build.gradle.kts
+++ green/build.gradle.kts
@@ -41,8 +41,8 @@ android {
     defaultConfig {
         minSdk = libs.versions.androidMinSdk.get().toInt()
         targetSdk = libs.versions.androidTargetSdk.get().toInt()
-        versionCode = 422
-        versionName = "4.0.22"
+        versionCode = 423
+        versionName = "4.0.23"
 
         setProperty("archivesBaseName", "BlockstreamGreen-v$versionName")
         proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
