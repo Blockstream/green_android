b85ba489af71fda29731476a64b510e1a88dd656
lilbonekit <mykytasydorenko0@gmail.com>
2026-01-14 20:52:57 +0200

    fix: add scroll for small screens in security level screen (#272)

--- compose/src/commonMain/kotlin/com/blockstream/compose/sheets/SecurityLevelBottomSheet.kt
+++ compose/src/commonMain/kotlin/com/blockstream/compose/sheets/SecurityLevelBottomSheet.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.SegmentedButton
@@ -111,7 +113,10 @@ fun SecurityLevelBottomSheet(
                 GreenColumn(
                     padding = 0,
                     space = 32,
-                    modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(),
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .fillMaxWidth()
+                        .verticalScroll(rememberScrollState()),
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
 
